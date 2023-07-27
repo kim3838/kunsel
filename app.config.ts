10843e2c01806735b7dbd325d22265ad77c972b7
Kim@PC <kguzman@abukai.com>
2023-07-27 19:57:12 +0800

    Install Fonts, Css, and Sample Images

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+
+export default defineAppConfig({
+    title: 'Hello Nuxt',
+    theme: {
+        dark: true,
+        colors: {
+            primary: '#ff0000'
+        }
+    },
+    array: () => ['bonjour'],
+})
