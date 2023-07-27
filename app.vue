6f1d8af77a7072be9e540cdf2d8f5e7481aabf30
Kim@PC <kguzman@abukai.com>
2023-07-27 18:02:12 +0800

    Update package-lock.json

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <h1 class="text-base tw-text-slate-500 font-medium">
+            Hello world!
+        </h1>
+    </div>
 </template>
