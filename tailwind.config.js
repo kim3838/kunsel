3df59b2c26b91a77ccc0f6c0812971fba40e088d
Kim@PC <kguzman@abukai.com>
2023-07-27 18:55:13 +0800

    Tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./components/Form/**/*.{js,vue,ts}",
+        "./components/Jetstream/**/*.{js,vue,ts}",
+        "./components/Logo/**/*.{js,vue,ts}",
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+    ],
+
+    prefix: 'tw-',
+
+    important: true,
+
+    theme: {
+        extend: {
+            fontFamily : {
+                sans : ['Sofia Sans Semi Condensed', 'Source Sans Pro', 'Barlow', 'Spline Sans', 'Roboto', 'Roboto Condensed', 'Inter', 'sans-serif'],
+                serif : ['Almendra', 'Oranienbaum', 'Instrument Serif', 'Gupter', 'Kotta One', 'serif'],
+                mono : ['Spline Sans Mono', 'Roboto Mono', 'monospace'],
+            },
+            spacing: {
+                'sidebar': '250px',
+                '2px' : '2px',
+                '5px' : '5px',
+                '12px' : '12px',
+                '1rem' : '1rem',
+                '1.3rem' : '1.3rem'
+            },
+            colors: {
+                'lighter': '#b4b4b4',
+                'light': '#969696',
+                'dark': '#787878',
+                'darker': '#323232',
+                'accent': '#646464',
+            }
+        },
+    },
+
+    plugins: [
+        require("@tailwindcss/forms")({
+            strategy: 'class',
+        }),
+    ],
+}
+
