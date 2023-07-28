d8a27c5a7c09bb8ae257379f53a9257f6c88b2d2
Kim@PC <kguzman@abukai.com>
2023-07-28 21:10:42 +0800

    Debug caller

--- app.vue
+++ app.vue
@@ -10,7 +10,8 @@
             <span v-text="monoParagraph"></span>
         </div>
 
-        <div class="tw-w-full tw-bg-gradient-to-r tw-from-transparent tw-via-gray-300 tw-from-transparent tw-my-1" style="height: 2px;"></div>
+        <div class="tw-w-full tw-bg-gradient-to-r tw-from-transparent tw-via-gray-300 tw-from-transparent tw-my-1"
+             style="height: 2px;"></div>
 
         <div class="tw-font-sans tw-text-base tw-text-gray-500">
             <span v-text="sansParagraph"></span>
@@ -22,7 +23,8 @@
             <span v-text="monoParagraph"></span>
         </div>
 
-        <div class="tw-w-full tw-bg-gradient-to-r tw-from-transparent tw-via-gray-300 tw-from-transparent tw-my-1" style="height: 2px;"></div>
+        <div class="tw-w-full tw-bg-gradient-to-r tw-from-transparent tw-via-gray-300 tw-from-transparent tw-my-1"
+             style="height: 2px;"></div>
 
         <div class="tw-text-sm tw-text-gray-500" style="font-family: 'Gupter', 'sans-serif';">
             <span v-text="serifParagraph"></span>
@@ -31,23 +33,24 @@
         <div class="tw-text-sm tw-text-gray-500" style="font-family: 'Kotta One', 'serif';">
             <span v-text="serifParagraph"></span>
         </div>
-        <div>{{useNuxtApp().$_.capitalize('hello world')}}</div>
-        <div>{{useNuxtApp().$moment().format('YYYY-MM-DD')}}</div>
     </div>
 </template>
 <script setup lang="ts">
-    const runtimeConfig = useRuntimeConfig();
-    const appConfig = useAppConfig()
-    console.log({runtimeConfig: runtimeConfig});
-    console.log({appConfig: appConfig});
+import {_debounce, _upperCase} from "#imports";
 
-    let lodashValue = useNuxtApp().$_.capitalize('hello world');
-    console.log(lodashValue);
+let sansParagraph: string = "Sans : The quick brown fox jumps over the lazy dog.";
+let serifParagraph: string = "Serif : The quick brown fox jumps over the lazy dog.";
+let monoParagraph: string = "Mono :The quick brown fox jumps over the lazy dog.";
 
-    const {$moment} = useNuxtApp();
-    console.log($moment().format('YYYY-MM-DD'));
+const { $debug, $moment} = useNuxtApp();
+const runtimeConfig = useRuntimeConfig();
+const appConfig = useAppConfig();
 
-    let sansParagraph: string = "Sans : The quick brown fox jumps over the lazy dog.";
-    let serifParagraph: string = "Serif : The quick brown fox jumps over the lazy dog.";
-    let monoParagraph: string = "Mono :The quick brown fox jumps over the lazy dog.";
+$debug("runtimeConfig: " + JSON.stringify(runtimeConfig));
+$debug("appConfig: " + JSON.stringify(appConfig));
+$debug($moment().format('YYYY-MM-DD'));
+$debug(_upperCase('lodash uppercase'));
+
+// let mountains = await $axios.get('https://api.nuxtjs.dev/mountains'+ '?secret=1');
+// console.log({mountains:mountains});
 </script>
