f4c0297394bcd04c46028d4ce124897643477b75
Kim@PC <kimdeguzman20@yahoo.com>
2023-07-30 08:51:44 +0800

    NavLink component & Add Navigation links

---
+++ components/NavLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <NuxtLink :to="to" :class="classes">
+        <slot></slot>
+    </NuxtLink>
+</template>
+
+<script>
+    export default {
+        props: ['to', 'active'],
+
+        computed: {
+            classes() {
+                return this.active
+                    ? 'tw-inline-flex tw-items-center tw-px-1 tw-pt-1 tw-border-b-2 tw-text-xl tw-font-base tw-font-serif tw-border-light tw-leading-4 focus:tw-outline-none tw-focus:border-gray-700 tw-transition tw-duration-150 tw-ease-in-out'
+                    : 'tw-inline-flex tw-items-center tw-px-1 tw-pt-1 tw-border-b-2 tw-text-xl tw-font-base tw-font-serif tw-border-transparent tw-leading-4 focus:tw-outline-none focus:tw-text-gray-700 focus:tw-border-gray-300 tw-transition tw-duration-150 tw-ease-in-out'
+            }
+        }
+    }
+</script>
---
+++ layouts/Custom.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <nav class="tw-border-b tw-border-neutral-200" :class="{'lg:tw-ml-sidebar': false}">
+            <!-- Primary Navigation Menu -->
+            <div class="tw-max-w-full tw-bg-white tw-mx-auto tw-px-2">
+                <div class="tw-flex tw-justify-center tw-h-10">
+                    <div class="tw-relative tw-flex">
+                        <!-- Navigation Links -->
+                        <div class="tw-hidden tw-space-x-4 lg:tw--my-px lg:tw-ml-10 lg:tw-flex">
+                            <NavLink :to="'/'" :active="isRouteActive('index')">
+                                Home
+                            </NavLink>
+                            <NavLink :to="'/login'" :active="isRouteActive('login')">
+                                Login
+                            </NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <main class="tw-relative">
+            <slot name="content"/>
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+function isRouteActive(routeSlug: string){
+    return [route.path, _toLower(route.name)].indexOf(_toLower(routeSlug)) >= 0;
+}
+</script>
