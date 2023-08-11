aa3d62f3f576e812bb80a2f7915cb70ea9b4b0af
Kim@PC <kimdeguzman20@yahoo.com>
2023-08-11 19:58:10 +0800

    nuxt.config.ts remove module "@sidebase/nuxt-auth"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,6 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
         '@pinia/nuxt',
-        '@sidebase/nuxt-auth',
         'nuxt-lodash',
         'nuxt-icon',
     ],
