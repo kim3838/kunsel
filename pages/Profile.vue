71525e6835ca9b3b3a153da311b26306c2d415fc
Kim@PC <kimdeguzman20@yahoo.com>
2023-08-14 19:20:24 +0800

    Profile page auth info table

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -2,15 +2,27 @@
     <div>
         <NuxtLayout name="custom">
             <template #content>
-                <div v-if="$authStore.isLoggedIn" class="tw-justify-end">
-                    <Button :disabled="pending" @click="handleLogout" :size="'md'"><span class="tw-font-semibold">Logout</span></Button>
+                <div class="tw-m-1">
+                    <div v-if="$authStore.isLoggedIn" class="tw-justify-end">
+                        <Button :disabled="pending" @click="handleLogout" :size="'sm'"><span class="tw-font-semibold">Logout</span></Button>
+                    </div>
+                    <table class="tw-mt-1 tw-border-collapse tw-tw-border tw-border-neutral-300">
+                        <tbody>
+                            <tr><td class="tw-border tw-border-neutral-300">Id</td><td class="tw-border tw-border-neutral-300">{{$authStore.user.id}}</td></tr>
+                            <tr><td class="tw-border tw-border-neutral-300">Name</td><td class="tw-border tw-border-neutral-300">{{$authStore.user.name}}</td></tr>
+                            <tr><td class="tw-border tw-border-neutral-300">Email</td><td class="tw-border tw-border-neutral-300">{{$authStore.user.email}}</td></tr>
+                            <tr><td class="tw-border tw-border-neutral-300">Email Verified at</td><td class="tw-border tw-border-neutral-300">{{$moment($authStore.user.email_verified_at).format('YYYY-MM-DD HH:mm:ss')}}</td></tr>
+                        </tbody>
+                    </table>
                 </div>
-
-                {{$authStore.user}}
             </template>
         </NuxtLayout>
     </div>
 </template>
+
+<style>
+
+</style>
 
 <script setup lang="ts">
 import {ref} from "vue";
@@ -20,7 +32,7 @@ definePageMeta({
     middleware: 'auth'
 });
 
-const { $authStore } = useNuxtApp();
+const { $moment, $authStore } = useNuxtApp();
 
 const pending = ref(false);
 
