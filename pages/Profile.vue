018caf218df5280f3a5ccf52faaff8a1a5789946
Kim@PC <kimdeguzman20@yahoo.com>
2023-08-15 21:23:32 +0800

    Fix logout Auth user store being null

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -4,14 +4,14 @@
             <template #content>
                 <div class="tw-m-1">
                     <div v-if="$authStore.isLoggedIn" class="tw-justify-end">
-                        <Button :disabled="pending" @click="handleLogout" :size="'sm'"><span class="tw-font-semibold">Logout</span></Button>
+                        <Button :disabled="pending" @click="handleLogout" :size="'md'"><span class="tw-font-semibold">Logout</span></Button>
                     </div>
                     <table class="tw-mt-1 tw-border-collapse tw-tw-border tw-border-neutral-300">
                         <tbody>
-                            <tr><td class="tw-border tw-border-neutral-300">Id</td><td class="tw-border tw-border-neutral-300">{{$authStore.user.id}}</td></tr>
-                            <tr><td class="tw-border tw-border-neutral-300">Name</td><td class="tw-border tw-border-neutral-300">{{$authStore.user.name}}</td></tr>
-                            <tr><td class="tw-border tw-border-neutral-300">Email</td><td class="tw-border tw-border-neutral-300">{{$authStore.user.email}}</td></tr>
-                            <tr><td class="tw-border tw-border-neutral-300">Email Verified at</td><td class="tw-border tw-border-neutral-300">{{$moment($authStore.user.email_verified_at).format('YYYY-MM-DD HH:mm:ss')}}</td></tr>
+                            <tr><td class="tw-border tw-border-neutral-300">Id</td><td class="tw-border tw-border-neutral-300">{{$authStore.user?.id}}</td></tr>
+                            <tr><td class="tw-border tw-border-neutral-300">Name</td><td class="tw-border tw-border-neutral-300">{{$authStore.user?.name}}</td></tr>
+                            <tr><td class="tw-border tw-border-neutral-300">Email</td><td class="tw-border tw-border-neutral-300">{{$authStore.user?.email}}</td></tr>
+                            <tr><td class="tw-border tw-border-neutral-300">Email Verified at</td><td class="tw-border tw-border-neutral-300">{{$moment($authStore.user?.email_verified_at).format('YYYY-MM-DD HH:mm:ss')}}</td></tr>
                         </tbody>
                     </table>
                 </div>
