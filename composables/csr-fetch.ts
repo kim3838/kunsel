262e0e1aad700c1f50e7c2d2039e3fc7af2bdc94
Kim@PC <kimdeguzman20@yahoo.com>
2023-08-17 20:16:16 +0800

    Client Side Rendering Fetch Composable

---
+++ composables/csr-fetch.ts
@@ -0,0 +1,28 @@
+
+import type {UseFetchOptions} from "nuxt/app";
+
+export function csrFetch<T>(path: string, options: UseFetchOptions<T> = {}){
+    const runtimeConfig = useRuntimeConfig();
+
+    let headers: any = {
+        referer: runtimeConfig.public.frontendURL,
+        accept: 'application/json, text/plain, */*'
+    };
+
+    const XSRF_TOKEN = useCookie('XSRF-TOKEN');
+
+    if(XSRF_TOKEN.value){
+        headers['X-XSRF-TOKEN'] = XSRF_TOKEN as String;
+    }
+
+    return useFetch(runtimeConfig.public.baseURL + path, {
+        credentials: 'include',
+        server: false,
+        lazy: true,
+        ...options,
+        headers: {
+            ...headers,
+            ...options?.headers
+        },
+    });
+}
