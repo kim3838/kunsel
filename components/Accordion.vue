38a115068ac81eb41037afeeebe12790205a11f4
Kim@PC <kimdeguzman20@yahoo.com>
2023-08-22 21:02:32 +0800

    Hero Header Class

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -13,16 +13,3 @@
         <div v-if="false" class="tw-text-lg">Show everyone you’re ready for battle when you slam your banner down, signaling to enemies that you’re ready for a fight. This emote is only included in the Accelerated Battle Pass.</div>
     </div>
 </template>
-
-<style scoped>
-.header-1{
-    filter:
-        drop-shadow(rgba(201, 201, 201, 0.5) 0px 0px 3px)
-        drop-shadow(#818181 0px 0px 3px)
-        drop-shadow(rgba(0, 0, 0, 0.3) 0px 1px 3px)
-        drop-shadow(rgba(0, 0, 0, 0.3) 0px 0px 3px)
-        brightness(1.6);
-    text-shadow: 1px 3px 3px rgba(0, 0, 0, 0.7);
-    text-transform: uppercase;
-}
-</style>
--- pages/Login.vue
+++ pages/Login.vue
@@ -5,52 +5,54 @@
                 <div class="tw-m-2 tw-p-2 tw-border tw-border-light">
 
                     <div class="tw-flex tw-justify-center tw-text-lg tw-text-gray-500">Premium Battle Pass</div>
-                    <div class="tw-flex tw-justify-center tw-tracking-wider tw-font-[Gupter] tw-text-4xl tw-font-semibold">GET MORE WITH PREMIUM</div>
+                    <div class="tw-flex tw-justify-center tw-tracking-wider header-1 tw-text-neutral-50 tw-font-[Gupter] tw-text-4xl tw-font-semibold">GET MORE WITH PREMIUM</div>
                     <div class="tw-flex tw-justify-center">
                         <p class="tw-tracking-wider tw-text-xl tw-text-gray-500 tw-max-w-[720px] tw-text-center">Earn your way through 90 tiers and unlock premium rewards like Awoken Armor Sets , Mounts & Mount Armor, Platinum, and much more.</p>
                     </div>
 
-                    <div class="tw-flex tw-border tw-border-neutral-200">
-                        <div class="moody-card tw-p-6">
-                            <div class="moody-card-frame"></div>
-                            <div class="tw-relative">
-                                <Accordion></Accordion>
+                    <div class="tw-flex tw-justify-center">
+                        <div class="tw-flex tw-border tw-border-neutral-200 tw-max-w-6xl">
+                            <div class="moody-card tw-p-6">
+                                <div class="moody-card-frame"></div>
+                                <div class="tw-relative">
+                                    <Accordion></Accordion>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="tw-p-6 tw-border tw-border-neutral-200">
-                            <form @submit.prevent="handleLogin" class="tw-w-72">
-                                <div class="tw-block">
-                                    <InputLabel :size="'sm'" for="email" value="Email" />
-                                    <Input :disabled="pending" :size="'sm'" id="email" type="email" class="tw-w-full" ref="emailInput" v-model="email" autofocus autocomplete="off" />
-                                    <sup>{{email}}</sup>
-                                </div>
+                            <div class="tw-x-2 tw-mt-8 tw-border tw-border-neutral-200">
+                                <form @submit.prevent="handleLogin" class="tw-w-72">
+                                    <div class="tw-block">
+                                        <InputLabel :size="'sm'" for="email" value="Email" />
+                                        <Input :disabled="pending" :size="'sm'" id="email" type="email" class="tw-w-full" ref="emailInput" v-model="email" autofocus autocomplete="off" />
+                                        <sup>{{email}}</sup>
+                                    </div>
 
-                                <div class="tw-block tw-mt-4">
-                                    <InputLabel :size="'md'" for="password" value="Password" />
-                                    <Input :disabled="pending" :size="'md'" id="password" type="password" class="tw-w-full" v-model="password" required autocomplete="current-password" />
-                                    <sup>{{password}}</sup>
-                                </div>
+                                    <div class="tw-block tw-mt-4">
+                                        <InputLabel :size="'md'" for="password" value="Password" />
+                                        <Input :disabled="pending" :size="'md'" id="password" type="password" class="tw-w-full" v-model="password" required autocomplete="current-password" />
+                                        <sup>{{password}}</sup>
+                                    </div>
 
-                                <div class="tw-block tw-mt-4">
-                                    <label class="tw-flex tw-items-center">
-                                        <Checkbox :disabled="pending" name="remember" v-model="remember" :size="'md'" :label="'Remember me'" />
-                                    </label>
-                                    <label>
-                                        <sup>{{remember}}</sup>
-                                    </label>
-                                </div>
+                                    <div class="tw-block tw-mt-4">
+                                        <label class="tw-flex tw-items-center">
+                                            <Checkbox :disabled="pending" name="remember" v-model="remember" :size="'md'" :label="'Remember me'" />
+                                        </label>
+                                        <label>
+                                            <sup>{{remember}}</sup>
+                                        </label>
+                                    </div>
 
-                                <div v-if="$coreStore.service.error.payload && !useNuxtApp().$coreStore.service.error.prompt" class="tw-block tw-text-sm tw-text-red-600">
-                                    <span>{{ $coreStore.service.error.payload.message }}</span>
-                                </div>
+                                    <div v-if="$coreStore.service.error.payload && !useNuxtApp().$coreStore.service.error.prompt" class="tw-block tw-text-sm tw-text-red-600">
+                                        <span>{{ $coreStore.service.error.payload.message }}</span>
+                                    </div>
 
-                                <div v-if="pending" class="tw-text-sm">Authenticating...</div>
+                                    <div v-if="pending" class="tw-text-sm">Authenticating...</div>
 
-                                <div v-if="!$authStore.isLoggedIn" class="tw-flex tw-items-center tw-justify-end">
-                                    <Button :disabled="pending" :size="'md'"><span class="tw-font-semibold">Authenticate</span></Button>
-                                </div>
-                            </form>
+                                    <div v-if="!$authStore.isLoggedIn" class="tw-flex tw-items-center tw-justify-end">
+                                        <Button :disabled="pending" :size="'md'"><span class="tw-font-semibold">Authenticate</span></Button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
 
