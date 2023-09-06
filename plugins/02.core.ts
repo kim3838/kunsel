01f28e5dd715b06d08f15199cc41cf8e9570d70d
Kim@PC <kimdeguzman20@yahoo.com>
2023-09-06 19:33:29 +0800

    Ordinal Helper

--- plugins/02.core.ts
+++ plugins/02.core.ts
@@ -6,4 +6,10 @@ export default defineNuxtPlugin(nuxtApp => {
         // @ts-ignore
         console.log({[`${useNuxtApp().$moment().format('YYYY-MM-DD H:m:s')} DEBUG: ${key}`]: payload})
     });
+
+    nuxtApp.provide('ordinal', function (number) {
+        let s = ["th", "st", "nd", "rd"];
+        let v = number%100;
+        return number + (s[(v-20)%10] || s[v] || s[0]);
+    });
 });
