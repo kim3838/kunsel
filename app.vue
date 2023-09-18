9f62a3ec92f05d633f0354de7dab67793ae66611
Kim@PC <kimdeguzman20@yahoo.com>
2023-09-18 20:15:43 +0800

    App: Fix Call Of Core Store State Value

--- app.vue
+++ app.vue
@@ -17,9 +17,8 @@ const appConfig = useAppConfig();
 // $debug('moment', $moment().format('YYYY-MM-DD'));
 // $debug('lodash', _upperCase('lodash uppercase'));
 
-// $coreStore.increment();
-// console.log($coreStore.$state.counter);
-// console.log($coreStore.doubleCounterPlusOne);
+// console.log({'layout': $coreStore.layout});
+// console.log({'layout': $coreStore.service.error.title});
 
 // const mountains = await $fetch('https://api.nuxtjs.dev/mountains').catch((error) => error.data)
 </script>
