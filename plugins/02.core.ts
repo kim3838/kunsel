5acbd3ece792f9fb5ff611b214dbd334cf6b3382
Kim@PC <kimdeguzman20@yahoo.com>
2023-09-24 13:09:46 +0800

    Core Plugin: Body Tag Bind and Watch Body Color on Mounted Hook

--- plugins/02.core.ts
+++ plugins/02.core.ts
@@ -1,7 +1,8 @@
 import {defineNuxtPlugin, useNuxtApp} from "#app";
+import {watch} from 'vue';
+import {storeToRefs} from 'pinia';
 
 export default defineNuxtPlugin(nuxtApp => {
-
     nuxtApp.provide('debug', function (key: string, payload: any) {
         // @ts-ignore
         console.log({[`${useNuxtApp().$moment().format('YYYY-MM-DD H:m:s')} DEBUG: ${key}`]: payload})
@@ -9,7 +10,26 @@ export default defineNuxtPlugin(nuxtApp => {
 
     nuxtApp.provide('ordinal', function (number) {
         let s = ["th", "st", "nd", "rd"];
-        let v = number%100;
-        return number + (s[(v-20)%10] || s[v] || s[0]);
+        let v = number % 100;
+        return number + (s[(v - 20) % 10] || s[v] || s[0]);
+    });
+
+    /*
+    * Runtime Hooks
+    * https://nuxt.com/docs/api/advanced/hooks#app-hooks-runtime
+    **/
+    nuxtApp.hook('app:mounted', () => {
+        const {$themeStore} = useNuxtApp();
+
+        const {
+            appTheme,
+            body: bodyColor,
+        } = storeToRefs($themeStore);
+
+        document.body.style.backgroundColor = bodyColor.value;
+
+        watch(appTheme, (newTheme) => {
+            document.body.style.backgroundColor = bodyColor.value;
+        });
     });
 });
