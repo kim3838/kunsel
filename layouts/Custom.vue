41c247c4fa19b038e66854bfa91615fb1d654f64
Kim@PC <kimdeguzman20@yahoo.com>
2023-09-26 20:46:45 +0800

    Custom Layout: Minor Changes & Additions
    - Auto Adjust Navigation Height On Window WidthResize
    - Bind Colors from Theme Store's storeToRefs

--- layouts/Custom.vue
+++ layouts/Custom.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="tw-relative">
-        <nav class="tw-border-b tw-border-neutral-200" :class="{'lg:tw-ml-sidebar': false}">
-            <!-- Primary Navigation Menu -->
-            <div class="tw-max-w-full tw-mx-auto tw-px-2 tw-bg-white dark:tw-bg-gray-800">
-                <div class="tw-flex tw-justify-center tw-h-10">
-                    <div class="tw-relative tw-flex">
+        <!-- Primary Navigation Menu -->
+        <nav ref="navigation" class="primary-navigation-parent tw-fixed" :class="{'lg:tw-ml-sidebar': false}">
+            <div class="tw-max-w-full tw-mx-auto">
+                <div class="tw-flex tw-justify-center tw-h-7 lg:tw-h-14">
+                    <div class="tw-flex">
                         <!-- Navigation Links -->
-                        <div class="tw-hidden tw-space-x-8 lg:tw--my-px lg:tw-ml-10 lg:tw-flex">
+                        <div class="tw-hidden tw-space-x-8 tw--my-px lg:tw-flex">
                             <NavLink :to="'/'" :active="isRouteActive('index')">
                                 Home
                             </NavLink>
@@ -30,7 +30,7 @@
                 </div>
             </div>
         </nav>
-        <main class="tw-relative">
+        <main class="tw-relative allocate-navigation">
             <slot name="content"/>
         </main>
         <ServiceErrorModal></ServiceErrorModal>
@@ -38,9 +38,59 @@
 </template>
 
 <script setup lang="ts">
+import {storeToRefs} from 'pinia';
+import {nextTick, onMounted, onUnmounted, ref} from "vue";
+const {$themeStore} = useNuxtApp();
 const route = useRoute();
+
+const {
+    neutral: neutralColor,
+    tint: tintColor,
+    lining: liningColor,
+    thread: threadColor
+} = storeToRefs($themeStore);
+
+let navigation = ref(null);
+let navigationHeight = ref('0px');
+let windowWidth = ref(0);
 
 function isRouteActive(routeSlug: string){
     return [route.path, _toLower(route.name)].indexOf(_toLower(routeSlug)) >= 0;
 }
+
+async function handleWindowWidth(){
+    windowWidth.value = window.innerWidth;
+    await nextTick(() => {
+        navigationHeight.value = `${navigation.value.offsetHeight}px`;
+    });
+}
+
+onMounted(async () => {
+    await nextTick(() => {
+        navigationHeight.value = `${navigation.value.offsetHeight}px`;
+    });
+    window.addEventListener('resize', handleWindowWidth);
+});
+
+onUnmounted(async () => {
+    window.removeEventListener('resize', handleWindowWidth);
+});
+
 </script>
+<style scoped>
+.primary-navigation-parent{
+    background-color: v-bind(tintColor) !important;
+    border-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: v-bind(neutralColor);
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    z-index: 30;
+}
+
+.allocate-navigation{
+    margin-top: v-bind(navigationHeight);
+}
+</style>
