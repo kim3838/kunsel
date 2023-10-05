2a7d1245c4e87544e7b7c2fbe88b1f526a26e268
Kim@PC <kimdeguzman20@yahoo.com>
2023-10-05 21:55:30 +0800

    InputWithIcon Component
    - Props In-Cell: Determine Top Absolute Allocation For Better Alignment

---
+++ components/form/InputWithIcon.vue
@@ -0,0 +1,190 @@
+<template>
+    <div class="tw-relative tw-box-border" :class="[heightClass]">
+        <div v-if="icon?.trim()" class="tw-absolute tw-h-full tw-z-40 tw-flex tw-items-center tw-pointer-events-none">
+            <Icon :class="[iconClass]" :name="icon" />
+        </div>
+        <input
+            :id="id"
+            :disabled="disabled"
+            :class="[
+                fontClass,
+                spacingClass,
+                backgroundClass,
+                'focus:tw-ring-transparent focus:tw-ring',
+                focusRing ? 'focus-ring' : '',
+                withBorder ? 'bordered' : 'borderless',
+                rounded ? 'tw-rounded-sm': ''
+            ]"
+            :style="{'top': absoluteTopAllocation}"
+            class="tw-relative tw-w-full tw-h-full tw-z-30 tw-form-input tw-box-border"
+            :value="modelValue"
+            :placeholder="placeholder"
+            :type="type"
+            :readonly="readonly"
+            @input="$emit('update:modelValue', $event.target.value)"
+            ref="input">
+    </div>
+
+</template>
+
+<script setup>
+import {ref, computed} from 'vue';
+import {storeToRefs} from 'pinia';
+const {$themeStore} = useNuxtApp();
+
+const {
+    lining: liningColor,
+    thread: threadColor,
+    tint: tintColor,
+} = storeToRefs($themeStore);
+
+const props = defineProps({
+    modelValue: [String, Number],
+    size: {
+        default: 'md'
+    },
+    id: {
+        type: String,
+        default: null,
+    },
+    icon: {
+        type: String,
+        default: null,
+    },
+    type: {
+        type: String,
+        default: 'text',
+    },
+    placeholder: {
+        type: String,
+        default: ''
+    },
+    inCell: {
+        type: Boolean,
+        default: false
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    readonly: {
+        type: Boolean,
+        default: false
+    },
+    ring: {
+        type: Boolean,
+        default: false
+    },
+    focusRing: {
+        type: Boolean,
+        default: true
+    },
+    withBorder: {
+        type: Boolean,
+        default: true
+    },
+    rounded: {
+        type: Boolean,
+        default: true
+    }
+});
+
+defineEmits(['update:modelValue']);
+
+const iconClass = computed(() => {
+    return {
+        '2xs': 'tw-h-[0.875rem] tw-w-[0.875rem] tw-ml-[0.2rem]',
+        'xs': 'tw-h-[1rem] tw-w-[1rem] tw-ml-[0.2rem]',
+        'sm': 'tw-h-[1rem] tw-w-[1rem] tw-ml-[0.4rem]',
+        'md': 'tw-h-[1.2rem] tw-w-[1.2rem] tw-ml-[0.4rem]',
+        'lg': 'tw-h-[1.875rem] tw-w-[1.875rem] tw-mt-[0.1rem] tw-ml-[0.4rem]',
+        'xl': 'tw-h-[2.25rem] tw-w-[2.25rem] tw-mt-[0.1rem] tw-ml-[0.4rem]',
+        '2xl': 'tw-h-[2.25rem] tw-w-[2.25rem] tw-mt-[0.1rem] tw-ml-[0.4rem]',
+    }[props.size]
+});
+
+const absoluteTopAllocation = computed(() => {
+    return {
+        '2xs': props.inCell ? '0' : '-0.2rem',
+        'xs': props.inCell ? '0' : '-1px',
+        'sm': '0',
+        'md': '0',
+        'lg': '0',
+        'xl': '0'
+    }[props.size]
+});
+
+const spacingClass = computed(() => {
+    return {
+        '2xs': props.icon?.trim() ? 'tw-pl-5' : '',
+        'xs': props.icon?.trim() ? 'tw-pl-5' : '',
+        'sm': props.icon?.trim() ? 'tw-pl-6' : '',
+        'md': props.icon?.trim() ? 'tw-pl-7' : '',
+        'lg': props.icon?.trim() ? 'tw-pl-10' : '',
+        'xl': props.icon?.trim() ? 'tw-pl-11' : ''
+    }[props.size]
+});
+
+const heightClass = computed(() => {
+    return {
+        '2xs': 'tw-h-5',
+        'xs': 'tw-h-6',
+        'sm': 'tw-h-7',
+        'md': 'tw-h-8',
+        'lg': 'tw-h-11',
+        'xl': 'tw-h-14',
+        '2xl' : 'tw-h-16',
+        '3xl' : 'tw-h-20',
+    }[props.size]
+});
+
+const fontClass = computed(() => {
+    return {
+        '2xs': 'tw-text-xs',
+        'xs': 'tw-text-xs',
+        'sm': 'tw-text-sm',
+        'md': 'tw-text-sm',
+        'lg': 'tw-text-lg tw-font-semibold',
+        'xl': 'tw-text-2xl tw-font-semibold',
+        '2xl': 'tw-text-3xl tw-font-bold',
+        '3xl': 'tw-text-5xl tw-font-bold',
+    }[props.size];
+});
+
+const backgroundClass = computed(() => {
+    return props.disabled ? '' : 'input-background';
+});
+
+</script>
+<style scoped>
+.input-background{
+    background-color: v-bind(tintColor) !important;
+}
+
+.focus-ring:focus{
+    border-color: v-bind(liningColor) !important;
+}
+
+.bordered{
+    border-width: 1px;
+    border-color: v-bind(threadColor) !important;
+}
+
+.borderless{
+    border: 1px solid transparent;
+}
+
+input[type=number] {
+    position: relative;
+}
+
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+    position: absolute;
+    opacity: 1;
+    top: 0;
+    right: 0;
+    height: 100%;
+}
+
+</style>
