23321ef74a435a50b6c1d410f68c3622ce4a67af
Kim@PC <kimdeguzman20@yahoo.com>
2023-10-07 10:10:42 +0800

    Input PlaceHolder Color Base From TextColor

--- app.vue
+++ app.vue
@@ -56,6 +56,15 @@ const {
     clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%);
 }
 
+::placeholder { /* Firefox */
+    color: v-bind(textColor) !important;
+    opacity: 1;
+}
+
+::-ms-input-placeholder { /* Edge 12-18 */
+    color: v-bind(textColor) !important;
+}
+
 .scaffold{
     border: 1px solid v-bind(liningColor);
 }
