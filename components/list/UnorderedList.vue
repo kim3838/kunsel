69ad96a03c87a8a0300677fe7cf94fe6c7d62f25
Kim@PC <kimdeguzman20@yahoo.com>
2023-10-09 21:11:41 +0800

    Un Ordered List Component

---
+++ components/list/UnorderedList.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="tw-relative tw-overflow-hidden tw-flex tw-items-start tw-justify-start">
+        <div class="tw-absolute tw-h-full tw-w-full tw-pointer-events-none"></div>
+        <div :style="{'height': iconSlotHeight}" class="tw-flex tw-items-center">
+            <Icon
+                :name="icon"
+                class="icon"
+                :class="[heightClass]"/>
+        </div>
+        <div :class="[fontClass]" class="tw-ml-1 tw-whitespace-pre-wrap">{{label}}</div>
+    </div>
+</template>
+
+<script setup>
+import {ref, computed} from 'vue';
+import {storeToRefs} from 'pinia';
+const {$themeStore} = useNuxtApp();
+
+const {
+    hexAlpha,
+    type: themeType,
+    primary: primaryColor,
+    lining: liningColor,
+    textInvert: textInvertColor,
+} = storeToRefs($themeStore);
+
+const iconColor = computed(() => {
+    return themeType.value == 'light'
+        ? (primaryColor.value + hexAlpha.value['70'])
+        : (textInvertColor.value);
+});
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: null,
+    },
+    size: {
+        type: String,
+        default: 'md'
+    },
+    icon: {
+        type: String,
+        default: 'material-symbols:arrow-right'
+    },
+});
+
+const iconSlotHeight = computed(()=>{
+    return {
+        'sm': '20px',
+        'md': '24px',
+        'lg': '28px',
+    }[props.size];
+})
+
+const heightClass = computed(() => {
+    return {
+        'sm': 'tw-h-3 tw-w-3',
+        'md': 'tw-h-3.5 tw-w-3.5',
+        'lg': 'tw-h-5 tw-w-5',
+    }[props.size];
+});
+
+const fontClass = computed(() => {
+    return {
+        'sm': 'tw-text-sm',
+        'md': 'tw-text-base',
+        'lg': 'tw-text-lg'
+    }[props.size];
+});
+</script>
+
+<style scoped>
+.icon {
+    color: v-bind(iconColor) !important;
+}
+</style>
