8c3c42a1f4951fcd0d6d4514b8467aa4623e24b3
Kim@PC <kimdeguzman20@yahoo.com>
2023-11-22 14:41:21 +0800

    Profile: Two-Factor

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -4,7 +4,7 @@
             <template #content>
                 <div class="tw-mx-auto tw-max-w-screen-2xl">
                     <div class="tw-mx-auto tw-flex tw-flex-col tw-justify-center tw-w-fit">
-                        <AccentFrame class="tw-my-4 tw-max-w-screen-md">
+                        <AccentFrame class="tw-my-4 tw-max-w-screen-sm">
                             <template #content>
                                 <div class="tw-relative">
                                     <p class="tw-font-medium tw-text-lg">Profile Information</p>
@@ -29,7 +29,7 @@
                             </template>
                         </AccentFrame>
 
-                        <form @submit.prevent="executeUpdatePassword" class="tw-max-w-screen-md tw-p-[1.5rem] neutral-border">
+                        <form @submit.prevent="executeUpdatePassword" class="tw-max-w-screen-sm tw-p-[1.5rem] neutral-border">
                             <p class="tw-font-medium tw-text-lg">Update Password</p>
                             <p class="tw-text-base">Ensure your account is using a long, random password to stay secure.</p>
 
@@ -53,8 +53,8 @@
                             </div>
                         </form>
 
-                        <form @submit.prevent="executeLogoutOtherDevice" class="tw-max-w-screen-md tw-mt-4 tw-p-[1.5rem] neutral-border">
-                            <p class="tw-font-medium tw-text-lg">Browser Sessions </p>
+                        <form @submit.prevent="executeLogoutOtherDevice" class="tw-max-w-screen-sm tw-mt-4 tw-p-[1.5rem] neutral-border">
+                            <p class="tw-font-medium tw-text-lg">Browser Sessions</p>
                             <p class="tw-text-base">Manage and log out your active sessions on other browsers and devices. </p>
 
                             <div v-if="sessions.length > 0" class="tw-mt-5 tw-space-y-6">
@@ -89,6 +89,66 @@
                             </div>
                         </form>
 
+                        <ClientOnly>
+                            <div class="tw-max-w-screen-sm tw-mt-4 tw-p-[1.5rem] neutral-border">
+                                <p class="tw-font-medium tw-text-lg">Two Factor Authentication</p>
+                                <p class="tw-text-base">Add additional security to your account using two factor authentication.</p>
+
+                                <div class="tw-mt-4 tw-pt-4 neutral-border-top">
+                                    <p v-if="twoFactorEnabled && twoFactorConfirmed" class="tw-font-medium tw-text-lg">You have enabled two factor authentication.</p>
+                                    <p v-else-if="twoFactorEnabled && !twoFactorConfirmed" class="tw-font-medium tw-text-lg">Finish enabling two factor authentication.</p>
+                                    <p v-else class="tw-font-medium tw-text-lg">You have not enabled two factor authentication. </p>
+
+                                    <p class="tw-text-base">When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from <b>your phone's Google Authenticator application</b>.</p>
+
+                                    <div v-if="twoFactorEnabled">
+                                        <p v-if="twoFactorConfirming" class="tw-mt-4 tw-text-sm">
+                                            To finish enabling two factor authentication, scan the following QR code using your phone's authenticator application or enter the setup key and provide the generated OTP code.
+                                        </p>
+
+                                        <div v-if="twoFactorConfirming">
+                                            <div v-if="qrCode" class="tw-mt-4 tw-p-2 tw-bg-white" v-html="qrCode" />
+                                            <div v-if="setupKey" class="tw-mt-4 tw-font-medium">
+                                                Setup Key: <span>{{setupKey}}</span>
+                                            </div>
+
+                                            <div class="tw-mt-4 tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2">
+                                                <div>
+                                                    <InputWithIcon :icon="'mdi:key-chain'" type="text" placeholder="Confirmation Code" v-model="twoFactorConfirmationCode" />
+                                                </div>
+                                                <div>
+                                                    <Button @click="executeConfirmTwoFactor" type="button" :disabled="confirmTwoFactorPending" :label="'Confirm'" />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div v-if="recoveryCodes.length > 0 && !twoFactorConfirming">
+                                            <div class="tw-mt-4 tw-text-sm">
+                                                <p class="tw-font-medium">
+                                                    Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.
+                                                </p>
+                                            </div>
+
+                                            <div class="tw-grid tw-gap-1 tw-mt-4 tw-px-4 tw-py-4 tw-font-mono tw-text-sm">
+                                                <div v-for="code in recoveryCodes" :key="code">
+                                                    {{ code }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="tw-mt-4 tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2">
+                                    <div>
+                                        <Button  v-if="twoFactorEnabled" @click="executeDisableTwoFactor" type="button" :disabled="disableTwoFactorPending" :label="'Disable 2 Factor Authentication'" />
+                                    </div>
+                                    <div>
+                                        <Button  v-if="!twoFactorEnabled" @click="executeEnableTwoFactor" type="button" :disabled="enableTwoFactorPending" :label="'Enable 2 Factor Authentication'" />
+                                    </div>
+                                </div>
+                            </div>
+                        </ClientOnly>
+
                     </div>
 
                 </div>
@@ -102,7 +162,8 @@
 </style>
 
 <script setup lang="ts">
-import {ref, reactive, watch} from 'vue';
+import {computed, ref, reactive, watch} from 'vue';
+
 const {$coreStore} = useNuxtApp();
 
 definePageMeta({
@@ -110,11 +171,13 @@ definePageMeta({
     middleware: 'auth'
 });
 
-let user = reactive({
+const user = ref({
     id: null,
     name: null,
     email: null,
     email_verified_at: null,
+    two_factor_enabled: false,
+    two_factor_confirmed: false,
 });
 
 let updatePassword = reactive({
@@ -136,18 +199,23 @@ await ssrFetch("/api/user", {
     onResponse({request, response, options}) {
 
         if(response._data.code == 200){
-            user = {
+            let two_factor_enabled = _get(response, '_data.values.two_factor_enabled', null)
+            let two_factor_confirmed = _get(response, '_data.values.two_factor_confirmed', null)
+
+            user.value = {
                 id: _get(response, '_data.values.id', null),
                 name: _get(response, '_data.values.name', null),
                 email: _get(response, '_data.values.email', null),
                 email_verified_at: _get(response, '_data.values.email_verified_at', null),
+                two_factor_enabled: two_factor_enabled,
+                two_factor_confirmed: two_factor_confirmed,
             };
         }
     }
 });
 
 let updatePasswordPending = ref(false);
-const {execute: executeUpdatePassword} = csrFetch("/update-password", {
+const {execute: executeUpdatePassword} = csrFetch("/api/update-password", {
     method: 'POST',
     body: updatePasswordDataComputed,
     immediate: false,
@@ -214,6 +282,138 @@ await csrFetch("/api/sessions", {
     method: 'GET',
     onResponse({request, response, options}) {
         sessions.value = _get(response, '_data.values', []);
+    }
+});
+
+const twoFactorEnabled = computed(() => user.value.two_factor_enabled);
+const twoFactorConfirmed = computed(() => user.value.two_factor_confirmed);
+const twoFactorConfirming = ref(false);
+const setupKey = ref(null);
+const qrCode = ref(null);
+const recoveryCodes = ref([]);
+const twoFactorConfirmationCode = ref('');
+
+await csrFetch("/api/user", {
+    method: 'GET',
+    onResponse({request, response, options}) {
+        if(response._data.code == 200){
+            let two_factor_enabled = _get(response, '_data.values.two_factor_enabled', null)
+            let two_factor_confirmed = _get(response, '_data.values.two_factor_confirmed', null)
+
+            if(two_factor_enabled && !two_factor_confirmed){
+                twoFactorConfirming.value = true;
+                executeTwoFactorSetupKey();
+                executeTwoFactorQrCode();
+                executeTwoFactorRecoveryCodes();
+            }
+
+            if(two_factor_enabled && two_factor_confirmed){
+                executeTwoFactorRecoveryCodes();
+            }
+        }
+    }
+});
+
+const enableTwoFactorPending = ref(false);
+const {execute: executeEnableTwoFactor} = csrFetch("/api/two-factor-authentication", {
+    method: 'POST',
+    immediate: false,
+}, {
+    onRequest: () => {
+        enableTwoFactorPending.value = true;
+    },
+    onRequestError: () => {
+        enableTwoFactorPending.value = false;
+    },
+    onResponse: () => {
+        enableTwoFactorPending.value = false;
+    },
+    onSuccessResponse: (request, response, options) => {
+        user.value.two_factor_enabled = true;
+        twoFactorConfirming.value = true;
+        executeTwoFactorSetupKey();
+        executeTwoFactorQrCode();
+        executeTwoFactorRecoveryCodes();
+    }
+});
+
+const {execute: executeTwoFactorSetupKey} = csrFetch("/api/two-factor-secret-key", {
+    method: 'GET',
+    immediate: false,
+}, {
+    onSuccessResponse: (request, response, options) => {
+        setupKey.value = _get(response, '_data.values.secret_key', null);
+    }
+});
+const {execute: executeTwoFactorQrCode} = csrFetch("/api/two-factor-qr-code", {
+    method: 'GET',
+    immediate: false,
+}, {
+    onSuccessResponse: (request, response, options) => {
+        qrCode.value = _get(response, '_data.values.svg', null);
+    }
+});
+const {execute: executeTwoFactorRecoveryCodes} = csrFetch("/api/two-factor-recovery-codes", {
+    method: 'GET',
+    immediate: false,
+}, {
+    onSuccessResponse: (request, response, options) => {
+        recoveryCodes.value = _get(response, '_data.values.recovery_codes', []);
+    }
+});
+
+const confirmTwoFactorPending = ref(false);
+const {execute: executeConfirmTwoFactor} = csrFetch("/api/confirmed-two-factor-authentication", {
+    method: 'POST',
+    body: {
+        'code': twoFactorConfirmationCode
+    },
+    immediate: false,
+}, {
+    onRequest: () => {
+        confirmTwoFactorPending.value = true;
+    },
+    onRequestError: () => {
+        confirmTwoFactorPending.value = false;
+    },
+    onResponse: () => {
+        confirmTwoFactorPending.value = false;
+    },
+    onSuccessResponse: (request, response, options) => {
+        twoFactorConfirming.value = false;
+        user.value.two_factor_confirmed = true;
+        setupKey.value = null;
+        qrCode.value = null;
+
+        $coreStore.setPrompt({
+            icon: 'ic:outline-mark-email-read',
+            title: 'Two-Factor Confirmed',
+            message: _get(response, '_data.message', ''),
+            action: {
+                label: 'Close'
+            }
+        });
+    }
+});
+
+const disableTwoFactorPending = ref(false);
+const {execute: executeDisableTwoFactor} = csrFetch("/api/two-factor-authentication", {
+    method: 'DELETE',
+    immediate: false,
+}, {
+    onRequest: () => {
+        disableTwoFactorPending.value = true;
+    },
+    onRequestError: () => {
+        disableTwoFactorPending.value = false;
+    },
+    onResponse: () => {
+        disableTwoFactorPending.value = false;
+    },
+    onSuccessResponse: (request, response, options) => {
+        user.value.two_factor_enabled = false;
+        user.value.two_factor_confirmed = false;
+        twoFactorConfirming.value = false;
     }
 });
 </script>
