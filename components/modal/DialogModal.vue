bbf84812f728ae2ff07ab487cd2daa8682ddc5ad
Kim@PC <kimdeguzman20@yahoo.com>
2023-11-23 17:08:11 +0800

    Dialog Modal

---
+++ components/modal/DialogModal.vue
@@ -0,0 +1,59 @@
+<template>
+    <Modal
+        :show="show"
+        :max-width="maxWidth"
+        :closeable="closeable"
+        @close="close"
+    >
+        <div class="tw-p-3 modal-body">
+            <div class="tw-text-lg tw-font-semibold">
+                <slot name="title" />
+            </div>
+
+            <div class="tw-mt-4 tw-text-base">
+                <slot name="content" />
+            </div>
+        </div>
+
+        <div class="tw-px-3 tw-py-2.5 tw-text-end">
+            <slot name="footer" />
+        </div>
+    </Modal>
+</template>
+
+<script setup>
+import {storeToRefs} from 'pinia';
+
+const {$themeStore} = useNuxtApp();
+
+const {
+    neutral: neutralColor,
+} = storeToRefs($themeStore);
+
+defineProps({
+    show: {
+        type: Boolean,
+        default: false,
+    },
+    maxWidth: {
+        type: String,
+        default: '2xl',
+    },
+    closeable: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const emit = defineEmits(['close']);
+
+const close = () => {
+    emit('close');
+};
+</script>
+<style scoped>
+
+.modal-body{
+    border-bottom:1px solid v-bind(neutralColor);
+}
+</style>
