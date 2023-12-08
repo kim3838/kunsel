63985025dbc4b4b863d6e34601ab365027801661
Kim@PC <kimdeguzman20@yahoo.com>
2023-12-08 17:49:44 +0800

    Accordion Detail Additions
    - Show active border only on single expand
    - Active border color default of $themeStore lining color
    - Active border color customizable via activeBorder props

--- components/accordion/Details.vue
+++ components/accordion/Details.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-        <div @click="activeComputed = !activeComputed" class="scaffold tw-px-2 tw-cursor-pointer tw-h-8 tw-flex tw-items-center">
+        <div
+            @click="activeComputed = !activeComputed"
+            :class="[heightClass, singleExpand && activeComputed ? 'active-border-title' : '']"
+            class="tw-px-2 tw-cursor-pointer tw-flex tw-items-center">
             <span class="tw-truncate tw-text-base">
                 <slot name="title"></slot>
             </span>
             <Icon class="tw-flex-none tw-h-5 tw-w-5" :name="'ic:baseline-arrow-drop-down'"></Icon>
         </div>
-        <div v-show="activeComputed" class="scaffold tw-px-2 tw-text-base">
+        <div
+            v-show="activeComputed"
+            :class="[singleExpand && activeComputed ? 'active-border-body' : '']"
+            class="tw-px-2 tw-text-base">
             <slot name="body"></slot>
         </div>
     </div>
@@ -14,6 +20,12 @@
 
 <script setup>
 import {computed} from "vue";
+import {storeToRefs} from 'pinia';
+const {$themeStore} = useNuxtApp();
+
+const {
+    lining: liningColor,
+} = storeToRefs($themeStore);
 
 const props = defineProps({
     size: {
@@ -25,7 +37,26 @@ const props = defineProps({
     active: {
         type: Boolean,
         default: false
-    }
+    },
+    activeBorder: {
+        type: String,
+        default: ''
+    },
+    singleExpand: {
+        type: Boolean,
+        default: false
+    },
+});
+
+const activeBorderComputed = computed(() => {
+    return props.activeBorder ? props.activeBorder : liningColor.value;
+});
+
+const heightClass = computed(() => {
+    return {
+        'md': 'tw-h-8',
+        'lg': 'tw-h-11'
+    }[props.size]
 });
 
 const emit = defineEmits(["toggle"]);
@@ -42,3 +73,14 @@ const activeComputed = computed({
     }
 });
 </script>
+<style scoped>
+.active-border-title {
+    border-width: 1px;
+    border-color: v-bind(activeBorderComputed) !important;
+}
+.active-border-body {
+    border-width: 1px;
+    border-color: v-bind(activeBorderComputed) !important;
+    border-top: none;
+}
+</style>
