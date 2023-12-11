34e4900d4b578a2cd35df7b099a55146a1b66379
Kim@PC <kimdeguzman20@yahoo.com>
2023-12-11 17:59:26 +0800

    Home: Carousel, New Arrivals and Latest News Grid

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,52 @@
     <div>
         <NuxtLayout :name="$coreStore.layout">
             <template #content>
-                <div class="tw-mx-auto tw-max-w-screen-2xl tw-flex tw-justify-center">
-                    <div class="tw-p-4 tw-flex tw-justify-center tw-space-x-2 tw-mx-auto">
-                        <div v-for="card in cards" :key="card">
+                <div class="tw-mx-auto tw-max-w-screen-2xl tw-space-y-8 tw-flex tw-flex-col">
+                    <article class="tw-mt-4">
+                        <AccentFrame>
+                            <template #content>
+                                <CarouselModuleCarousel class="tw-mt-4" wrap-around :autoplay="4000">
+                                    <CarouselModuleSlide v-for="slide in carouselPayload" :key="slide">
+                                        <div class="tw-w-screen tw-h-[720px] carousel-image" :style="{'background-image': `url(${slide.image.path})`}"></div>
+                                    </CarouselModuleSlide>
+
+                                    <template #addons>
+                                        <CarouselModuleNavigation />
+                                        <CarouselModulePagination />
+                                    </template>
+                                </CarouselModuleCarousel>
+                            </template>
+                        </AccentFrame>
+                    </article>
+
+
+                    <article>
+                        <div class="tw-text-center tw-text-xl">New Arrivals</div>
+                        <div class="tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 md:tw-grid-cols-3 lg:tw-grid-cols-3 xl:tw-grid-cols-5 tw-gap-2">
                             <Card
-                                :image="card.image"
-                                :title="card.title"
-                                :sub-title="card.subTitle"
-                                :link="card.link"
+                                class="tw-inline-block tw-w-full"
+                                v-for="newArrival in newArrivals"
+                                :key="newArrival"
+                                :image="newArrival.image"
+                                :title="newArrival.title"
+                                :sub-title="newArrival.subTitle"
+                                :link="newArrival.link"
                             />
                         </div>
-                    </div>
+                    </article>
+
+                    <article>
+                        <div class="tw-text-center tw-text-xl">Latest News</div>
+                        <div class="tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 md:tw-grid-cols-3 lg:tw-grid-cols-3 xl:tw-grid-cols-5 tw-gap-2">
+                            <div v-for="blog in news" class="neutral-border">
+                                <figure class="figure">
+                                    <img :src="blog.image.path" />
+                                </figure>
+                                <div class="tw-px-4 tw-pt-2 tw-text-lg tw-font-semibold tw-tracking-normal tw-leading-tigh">{{blog.title}}</div>
+                                <div class="tw-px-4 tw-pt-2 tw-text-base tw-line-clamp-4 tw-leading-tight">{{blog.subTitle}}</div>
+                            </div>
+                        </div>
+                    </article>
                 </div>
             </template>
         </NuxtLayout>
@@ -21,6 +56,7 @@
 
 <script setup lang="ts">
 import {ref,reactive} from "vue";
+
 definePageMeta({
     layout: false,
     middleware: [
@@ -29,7 +65,7 @@ definePageMeta({
     ]
 });
 
-let cards = reactive([
+let newArrivals = reactive([
     {
         'image':{
             'path': '/images/hero/aa5b582b-908b-4db3-b5bc-1276f062d609.webp'
@@ -71,4 +107,78 @@ let cards = reactive([
         'link' : '/prototype'
     },
 ]);
+let news = reactive([
+    {
+        'image':{
+            'path': '/images/b38f4a68-129d-44c7-bd5b-35b7349d4166.webp'
+        },
+        'title': 'Seize the Season With RTX',
+        'subTitle': 'Celebrate the holiday season with the ultimate upgrade.Treat yourself to unrivaled performance and features with GeForce RTX™ 40 Series graphics cards and laptops built to transform every experience.',
+    },
+    {
+        'image':{
+            'path': '/images/68fc854c-e748-49a8-8388-6efa64f59239.webp'
+        },
+        'title': 'COLORFUL Launches B760 Series Motherboards',
+        'subTitle': 'COLORFUL introduces the CVN B760M FROZEN WIFI D5 and CVN B760I FROZEN WIFI motherboards for gamers and enthusiasts. Coming in a compact micro-ATX and mini-ITX form factors, both motherboards feature PCIe 5.0 x16 slot to support the latest high-end graphics cards. The CVN B760 motherboards also come with Wi-Fi 6, three PCIe 4.0 M.2 slots for the CVN B760M FROZEN WIFI D5 and two PCIe 4.0 M.2 slots for the CVN B760I FROZEN WIFI mini-ITX motherboard. COLORFUL also presents the BATTLE-AX B760M-F PRO motherboard with DDR4 memory support and two PCIe 4.0 M.2 slots.',
+    },
+    {
+        'image':{
+            'path': '/images/e9be486f-6a38-4d04-90e2-c5cf7c67f34f.webp'
+        },
+        'title': 'Further With AI, Faster on RTX',
+        'subTitle': 'From enhanced creativity + ultra-efficient productivity to blisteringly fast gaming. The ultimate in AI power on Windows PCs is on NVIDIA RTX and GeForce RTX.',
+    },
+    {
+        'image':{
+            'path': 'images/a447b48a-2ed6-4c7b-ad81-a7159fd341a7.webp'
+        },
+        'title': 'PC Game Pass Game Bundle',
+        'subTitle': 'Get PC Game Pass for 3 months with select GeForce RTX 40 Series*',
+    },
+    {
+        'image':{
+            'path': 'images/ff4e09cf-c5f9-42fa-9bff-e1490dc1e2fa.webp'
+        },
+        'title': 'COLORFUL Launches EVOL X15 AT Gaming Laptop Powered by Intel 13th Gen CPUs and NVIDIA GeForce RTX 4060 GPU',
+        'subTitle': 'The EVOL X15 AT sports a 15.6” QHD (2560x1440) 165Hz display with NVIDIA G-SYNC support for stutter-free and tear-free gaming experience. Aside from Pine Blue and Mist Grey, the new EVOL X15 AT gaming laptop comes in the new Purple Grape color.',
+    },
+]);
+let carouselPayload = reactive([
+    {
+        'image': {
+            'path': '/images/carousel/f415740d-5951-4b5d-af98-1404e9c65df5.jpg'
+        }
+    },
+    {
+        'image': {
+            'path': '/images/carousel/60a0ac44-4533-421b-aa6c-cfba7036fb97.webp'
+        }
+    },
+    {
+        'image': {
+            'path': '/images/carousel/81dbd81d-596d-4500-87d1-1e4fe1b9462c.webp'
+        }
+    },
+    {
+        'image': {
+            'path': '/images/carousel/202ae924-b777-45a2-94c6-f78134b17020.webp'
+        }
+    },
+    {
+        'image': {
+            'path': '/images/carousel/2271fbba-86bf-4dc7-93cb-59eb0aab62d8.webp'
+        }
+    }
+]);
 </script>
+<style scoped>
+.figure img{
+    object-fit: cover;
+}
+.carousel-image{
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
