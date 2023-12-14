e4d0ae124c9ecf760387c6703e5f6e01a110d8f5
Kim@PC <kimdeguzman20@yahoo.com>
2023-12-14 15:59:27 +0800

    Add Sample Logos

---
+++ components/logo/MediaCorp.vue
@@ -0,0 +1,83 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" height="100%" width="100%" xml:space="preserve" y="0" x="0" id="Layer_1"
+         version="1.1" viewBox="-47.55 -11.125 412.1 66.75">
+        <g id="g10647"><path id="path10529" d="M142.4 33.4h-5.2V13.6c2.3-1.3 5.8-2 9.1-2 2.3 0 4 .5 5.1 1.2 1.9-.7 4.2-1.2 6.4-1.2 6.8 0 7.8 3.7 7.8 8.1v13.7h-5.3V20.1c0-2.8-.7-4.3-3.6-4.3-.8 0-2.1.2-3 .7.2 1 .3 2 .3 3.1v13.7h-5.3V20.1c0-2.7-.6-4.3-3.5-4.3-.9 0-2.2.2-2.8.6v17z" class="st0"/>
+            <path id="path10531"
+                  d="M169.4 22.6c0-7.8 2.4-11.1 9.5-11.1 7.7 0 9.3 4.1 9.3 10.6 0 .6 0 1.1-.1 1.7-2.4.4-7.1.9-10 1.1l-3.5.2c.2 3.4 1.9 4.2 5 4.2 2.8 0 5.4-.3 7.6-.6l.4 4.1c-2.2.4-5.1.8-9.2.8-7.1.1-9-4.6-9-11zm8.7-1.3c1.4-.1 3.5-.3 4.7-.4V20c0-2.8-1-4.4-4-4.4-3.9 0-4.4 2.5-4.4 5.8z"
+                  class="st0"/>
+            <path id="path10533"
+                  d="M204.7 3.5h5.3v28.1c-2.4 1.4-5.8 2-8.7 2-7 0-10-2.6-10-11.1 0-8.3 2.9-11.1 9.4-11.1 1.3 0 2.8.2 4 .5zm-3 12.3c-5 0-5.2 2.9-5.2 6.8 0 3.5.2 6.9 4.7 6.9 1.2 0 2.6-.2 3.4-.6V16.3c-.8-.3-2-.5-2.9-.5z"
+                  class="st0"/>
+            <path id="path10535"
+                  d="M214 6.3c0-2.2.7-3 3.1-3 2.3 0 3.1.8 3.1 3s-.7 3-3.1 3c-2.4 0-3.1-.8-3.1-3zm.5 5.5h5.3v21.5h-5.3z"
+                  class="st0"/>
+            <path id="path10537"
+                  d="M223.7 26.8c0-3.8 1.6-6.2 5.8-6.8 2.2-.4 4.4-.4 6.8-.5 0-2.5-1.1-3.6-3.9-3.6-2.5 0-5.3.3-7.5.6l-.4-4c2.2-.4 5.2-.8 8.3-.8 4.7 0 8.6 1.3 8.6 6.9v13.2c-2.3 1.3-5.8 2-9.6 2-5.8-.1-8.1-2.4-8.1-7zm12.6-3.9c-1.6.1-3.4.1-4.9.5-1.8.4-2.6 1.1-2.6 3.3 0 2.3 1.3 2.9 3.6 2.9 1.2 0 2.7-.1 3.9-.6v-6.1z"
+                  class="st0"/>
+            <path id="path10539"
+                  d="M260.3 32.8c-1.8.5-4.1.8-6.9.8-7.1 0-8.2-5.5-8.2-11.3 0-7 1.6-10.9 9.4-10.9 2.4 0 4.2.4 5.7.8l-.4 3.9c-1.3-.3-2.5-.5-3.8-.5-4.9 0-5.5 1.5-5.5 6.6 0 4.8.2 6.9 5.5 6.9 1.3 0 2.5-.2 3.8-.5z"
+                  class="st0"/>
+            <path id="path10541"
+                  d="M262.3 22.6c0-8 2.5-11.1 9.5-11.1s9.5 3.1 9.5 11.1-2.5 11.1-9.5 11.1-9.5-3-9.5-11.1zm13.6 0c0-5.2-.9-6.7-4.2-6.7-3.3 0-4.2 1.4-4.2 6.7 0 5.4.9 6.7 4.2 6.7 3.4 0 4.2-1.5 4.2-6.7z"
+                  class="st0"/>
+            <path id="path10543"
+                  d="M284.8 33.4V13.6c2.4-1.4 5.8-2 8.7-2 .8 0 1.5 0 2.1.1l-.4 4.3c-.4-.1-1-.1-1.5-.1-1.2 0-2.8.2-3.6.7v16.8z"
+                  class="st0"/>
+            <path id="path10545"
+                  d="M303.6 41.3h-5.3V13.5c2.4-1.4 5.8-2 8.7-2 7 0 10 2.6 10 11.1 0 8.3-2.9 11.1-9.4 11.1-1.3 0-2.8-.2-4-.5zm3-11.9c5 0 5.2-2.9 5.2-6.8 0-3.5-.2-6.9-4.7-6.9-1.2 0-2.6.2-3.4.6v12.6c.8.3 1.9.5 2.9.5z"
+                  class="st0"/>
+            <linearGradient y2="11.1342" x2="37.8086" y1="11.1342" x1="12.6028" gradientUnits="userSpaceOnUse"
+                            id="SVGID_1_"><stop id="stop10547" offset=".1953" stop-color="#ec008c"/>
+                <stop id="stop10549" offset="1" stop-color="#2e3192"/></linearGradient>
+            <path id="polygon10552" fill="url(#SVGID_1_)" d="M18.9 11.1L25.2 0l6.3 11.1 6.3 11.2H12.6z"/>
+            <linearGradient y2="22.2685" x2="75.6169" y1="44.5375" x1="75.6169" gradientUnits="userSpaceOnUse"
+                            id="SVGID_2_"><stop id="stop10554" offset=".1" stop-color="#bcd85f"/>
+                <stop id="stop10556" offset=".1956" stop-color="#b3d35e"/>
+                <stop id="stop10558" offset=".3515" stop-color="#9bc45b"/>
+                <stop id="stop10560" offset=".5484" stop-color="#73ac57"/>
+                <stop id="stop10562" offset=".7762" stop-color="#3c8b51"/>
+                <stop id="stop10564" offset="1" stop-color="#00664b"/></linearGradient>
+            <path id="polygon10567" fill="url(#SVGID_2_)" d="M81.9 33.4l-6.3 11.1-6.3-11.1L63 22.3h25.2z"/>
+            <linearGradient y2="11.1342" x2="88.2196" y1="11.1342" x1="63.0142" gradientUnits="userSpaceOnUse"
+                            id="SVGID_3_"><stop id="stop10569" offset=".1" stop-color="#bcd85f"/>
+                <stop id="stop10571" offset=".1956" stop-color="#b3d35e"/>
+                <stop id="stop10573" offset=".3515" stop-color="#9bc45b"/>
+                <stop id="stop10575" offset=".5484" stop-color="#73ac57"/>
+                <stop id="stop10577" offset=".7762" stop-color="#3c8b51"/>
+                <stop id="stop10579" offset="1" stop-color="#00664b"/></linearGradient>
+            <path id="polygon10582" fill="url(#SVGID_3_)" d="M69.3 11.1L75.6 0l6.3 11.1 6.3 11.2H63z"/>
+            <linearGradient y2="22.2685" x2="56.7127" y1="44.5375" x1="56.7127" gradientUnits="userSpaceOnUse"
+                            id="SVGID_4_"><stop id="stop10584" offset="0" stop-color="#fbb040"/>
+                <stop id="stop10586" offset=".5" stop-color="#db801e"/>
+                <stop id="stop10588" offset=".5999" stop-color="#d77220"/>
+                <stop id="stop10590" offset=".7878" stop-color="#cc4e26"/>
+                <stop id="stop10592" offset="1" stop-color="#be1e2d"/></linearGradient>
+            <path id="polygon10595" fill="url(#SVGID_4_)" d="M75.6 44.5H50.4L37.8 22.3H63z"/>
+            <linearGradient y2="22.2685" x2="107.124" y1="44.5375" x1="107.124" gradientUnits="userSpaceOnUse"
+                            id="SVGID_5_"><stop id="stop10597" offset=".2011" stop-color="#49c0b6"/>
+                <stop id="stop10599" offset=".3893" stop-color="#3db3b7"/>
+                <stop id="stop10601" offset=".7379" stop-color="#1c90ba"/>
+                <stop id="stop10603" offset="1" stop-color="#0072bc"/></linearGradient>
+            <path id="polygon10606" fill="url(#SVGID_5_)" d="M126 44.5h-25.2L88.2 22.3h25.2z"/>
+            <linearGradient y2="22.2685" x2="18.9042" y1="44.5375" x1="18.9042" gradientUnits="userSpaceOnUse"
+                            id="SVGID_6_"><stop id="stop10608" offset="0" stop-color="#ec008c"/>
+                <stop id="stop10610" offset=".1439" stop-color="#ca098d"/>
+                <stop id="stop10612" offset=".4453" stop-color="#871a8f"/>
+                <stop id="stop10614" offset=".6988" stop-color="#572691"/>
+                <stop id="stop10616" offset=".8912" stop-color="#392e92"/>
+                <stop id="stop10618" offset="1" stop-color="#2e3192"/></linearGradient>
+            <path id="polygon10621" fill="url(#SVGID_6_)" d="M0 44.5h25.2l12.6-22.2H12.6z"/>
+            <linearGradient y2="11.1342" x2="113.4254" y1="11.1342" x1="75.6167" gradientUnits="userSpaceOnUse"
+                            id="SVGID_7_"><stop id="stop10623" offset=".2011" stop-color="#49c0b6"/>
+                <stop id="stop10625" offset=".3893" stop-color="#3db3b7"/>
+                <stop id="stop10627" offset=".7379" stop-color="#1c90ba"/>
+                <stop id="stop10629" offset="1" stop-color="#0072bc"/></linearGradient>
+            <path id="polygon10632" fill="url(#SVGID_7_)" d="M113.4 22.3H88.2L75.6 0h25.2z"/>
+            <linearGradient y2="11.1342" x2="63.0142" y1="11.1342" x1="25.2057" gradientUnits="userSpaceOnUse"
+                            id="SVGID_8_"><stop id="stop10634" offset="0" stop-color="#fbb040"/>
+                <stop id="stop10636" offset=".5" stop-color="#db801e"/>
+                <stop id="stop10638" offset=".5999" stop-color="#d77220"/>
+                <stop id="stop10640" offset=".7878" stop-color="#cc4e26"/>
+                <stop id="stop10642" offset="1" stop-color="#be1e2d"/></linearGradient>
+            <path id="polygon10645" fill="url(#SVGID_8_)" d="M63 22.3H37.8L25.2 0h25.2z"/></g></svg>
+</template>
