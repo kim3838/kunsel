fee87799a2ffb96d76024ca6659e151069d8899a
Kim@PC <kimdeguzman20@yahoo.com>
2024-01-13 14:46:47 +0800

    HexagonFrame: Border Shimmer Effect Update

--- components/frame/HexagonFrame.vue
+++ components/frame/HexagonFrame.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="tw-relative tw-w-full tw-h-full">
         <div class="clip"></div>
-        <div class="clip-frame tw-z-30 neutral-border">
+        <div class="clip-frame tw-z-30 thread-border tw-flex tw-justify-end">
             <slot name="body"></slot>
         </div>
-        <div class="clip-inner tint-background tw-z-20"><!--tint-background-->
+        <div class="clip-inner tint-background tw-z-20">
+            <slot name="head"></slot>
         </div>
     </div>
 </template>
@@ -16,6 +17,7 @@ const {$themeStore} = useNuxtApp();
 const {
     hexAlpha,
     primary: primaryColor,
+    thread: threadColor,
     lining: liningColor,
 } = storeToRefs($themeStore);
 </script>
@@ -23,28 +25,22 @@ const {
 <style scoped lang="scss">
 @keyframes animate-border {
     0%{background-position: 0% 0%;}
-    //25%{background-position: 25% 0%;}
-    50%{background-position: 100% 0%;}
-    //75%{background-position: 75% 0%;}
+    50%{background-position: 600% 0%;}
     100%{background-position: 0% 0%;}
 }
 .clip{
     width: 100%;
     height: 100%;
-    clip-path: polygon(calc(100% - 73px) 0px, 100% 73px, 100% 100%, 53px 100%, 0 calc(100% - 53px), 0 0);
-    background-size: 200% 100%;
+    clip-path: polygon(calc(100% - 75px) 0px, 100% 75px, 100% 100%, 55px 100%, 0 calc(100% - 55px), 0 0);
+    background-size: 150% 100%;
     background-position: 0% 0;
-    background-repeat: no-repeat;
+    background-repeat: repeat;
     background-image: linear-gradient(
-            135deg,
-            v-bind(liningColor) 20%,
-            transparent 30%,
-            transparent 40%,
-            v-bind(liningColor) 50%,
-            v-bind(liningColor) 60%,
-            transparent 70%,
-            transparent 80%,
-            v-bind(liningColor) 90%);
+            112deg,
+            v-bind(liningColor) 40%,
+            transparent 50%,
+            transparent 55%,
+            v-bind(liningColor) 65%);
     -webkit-animation: animate-border 10s infinite ease;
     -moz-animation: animate-border 10s infinite ease;
     animation: animate-border 10s infinite ease;
@@ -58,7 +54,7 @@ const {
     bottom: 7px;
 }
 .clip-inner{
-    clip-path: polygon(calc(100% - 75px) 2px, calc(100% - 2px) 75px, calc(100% - 2px) calc(100% - 2px), 55px calc(100% - 2px), 2px calc(100% - 55px), 2px 2px);
+    clip-path: polygon(calc(100% - 75px) 1px, calc(100% - 1px) 75px, calc(100% - 1px) calc(100% - 1px), 55px calc(100% - 1px), 1px calc(100% - 55px), 1px 1px);
     position: absolute;
     top: 0;
     left: 0;
