9747ec8723a93e26e7354726bff1a5ec41868d0e
Kim@PC <kimdeguzman20@yahoo.com>
2024-03-09 11:09:53 +0800

    Global Layout Middleware: Boot Layout Features

--- middleware/01.layout.global.ts
+++ middleware/01.layout.global.ts
@@ -3,15 +3,19 @@ export default defineNuxtRouteMiddleware((to, from) => {
         'name' : _lowerCase(to.name)
     };
 
-    if(_includes(['index','login'],_lowerCase(to.name))){
-        setLayout('landing');
-    } else {
-        setLayout('default');
-    }
+    const {
+        setNavigationMode,
+    } = useLayout();
 
     if(_includes(['index'],_lowerCase(to.name))){
         setNavigationMode('clear');
     } else {
         setNavigationMode('solid');
+    }
+
+    if(_includes(['index','login'],_lowerCase(to.name))){
+        setLayout('landing');
+    } else {
+        setLayout('default');
     }
 })
