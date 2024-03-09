517e5dd73fbba5e0ee1c84108e6fb6b6b6a10f30
Kim@PC <kimdeguzman20@yahoo.com>
2024-03-09 11:10:41 +0800

    Runtime App Hooks Middleware

--- plugins/04.runtime-app-hooks.ts
+++ plugins/04.runtime-app-hooks.ts
@@ -6,34 +6,32 @@ export default defineNuxtPlugin((nuxtApp) => {
 
     //Called when the setup() of the new page is running.
     nuxtApp.hook('page:loading:start', () => {
-        console.log({'RUNTIME APP HOOK': 'page:loading:start'});
+        //console.log({'RUNTIME APP HOOK': 'page:loading:start'});
     });
 
     //Called when initial vueApp instance is created.
     nuxtApp.hook('app:created', () => {
-        console.log({'RUNTIME APP HOOK': 'app:created'});
-        const route = useRoute();
-
-        if(['index'].indexOf(_toLower(route.name)) >= 0){
-            useLayout().setNavigationMode('clear');
-        } else {
-            useLayout().setNavigationMode('solid');
-        }
+        //console.log({'RUNTIME APP HOOK': 'app:created'});
     });
 
     //Called when SSR rendering is done.
     nuxtApp.hook('app:rendered', () => {
-        console.log({'RUNTIME APP HOOK': 'app:rendered'});
+        //console.log({'RUNTIME APP HOOK': 'app:rendered'});
     });
 
     //Called on Suspense pending event.
     nuxtApp.hook('page:start', () => {
-        console.log({'RUNTIME APP HOOK': 'page:start'});
+        //console.log({'RUNTIME APP HOOK': 'page:start'});
+    });
+
+    //Called on Suspense resolved event.
+    nuxtApp.hook('page:finish', () => {
+        //console.log({'RUNTIME APP HOOK': 'page:finish'});
     });
 
     //Called after page:finish
     nuxtApp.hook('page:loading:end', () => {
-        console.log({'RUNTIME APP HOOK': 'page:loading:end'});
+        //console.log({'RUNTIME APP HOOK': 'page:loading:end'});
     });
 })
 
