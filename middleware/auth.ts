c244813a91bffb9c2b77bdc40a539485fc244776
Kim@PC <kimdeguzman20@yahoo.com>
2024-03-09 11:08:59 +0800

    Auth Middleware: Boot Layout Components
    - Set layout & navigation mode for before redirecting to login page

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,13 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = userState();
+    const {
+        setNavigationMode,
+    } = useLayout();
 
     if(!user.value && to.path !== '/login'){
+        setNavigationMode('solid');
+        setLayout('landing');
+
         return navigateTo("/login", {replace: true});
     }
 })
