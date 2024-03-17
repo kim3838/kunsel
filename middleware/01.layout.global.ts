bd7af7802c8553522562e4eba76be7fb4fab68b0
Kim@PC <kimdeguzman20@yahoo.com>
2024-03-17 12:22:33 +0800

    Middleware Global Layout Minor Changes / Additions
    - Fix Route to.name Lowercase Assignment
    - Additional layouts for route names

--- middleware/01.layout.global.ts
+++ middleware/01.layout.global.ts
@@ -1,22 +1,28 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     useRouteTo().value = {
-        'name' : _lowerCase(to.name)
+        'name' : to.name.toLowerCase()
     };
 
     const {
         setNavigationMode,
     } = useLayout();
 
-    if(!_includes(['index'],_lowerCase(to.name))){
+    if(!_includes(['index'], to.name.toLowerCase())){
         setNavigationMode('solid');
     }
 
-    switch (_lowerCase(to.name)) {
+    switch (to.name.toLowerCase()) {
         case 'index':
             setNavigationMode('clear');
             setLayout('snap-landing');
             break;
         case 'login':
+            setLayout('landing');
+            break;
+        case 'forgot-password':
+            setLayout('landing');
+            break;
+        case 'two-factor-challenge':
             setLayout('landing');
             break;
         default:setLayout('default');
