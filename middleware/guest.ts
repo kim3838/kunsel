013cdf5ca5b52d3ba216d938e0cafddbe3518a87
Kim@PC <kimdeguzman20@yahoo.com>
2024-04-13 21:49:21 +0800

    Remove Guest Middleware

--- middleware/guest.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const user = userState();
-
-    if(user.value && to.path !== '/'){
-        return navigateTo("/", {replace: true});
-    }
-})
