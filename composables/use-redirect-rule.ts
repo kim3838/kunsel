9781466a962192fffa596a8a173e9144ca6ab3eb
Kim@PC <kimdeguzman20@yahoo.com>
2024-04-28 18:45:38 +0800

    Update Redirect Rule Composable

--- composables/use-redirect-rule.ts
+++ composables/use-redirect-rule.ts
@@ -5,34 +5,31 @@ export const redirectRuleMapState = () => {
     return useState('redirect-rule-map', () => {
         return {
             'guest' : ['lab', 'prototype', 'example', 'login', 'forgot-password', 'two-factor-challenge', 'password-reset-token'],
-            'sample-middleware' : ['index', 'prototype', 'example']
+            'skipable-middleware' : ['index', 'prototype', 'example']
         }
     });
 };
 
 export const bootRedirectRule = async function(rules = []){
-    console.log('BEFORE bootRedirectRule clientReadyState: ' +useClientReadyState().value );
-    console.log('bootRedirectRule ClientReadyState = false');
     useClientReadyState().value = false;
-    console.log('AFTER bootRedirectRule clientReadyState: ' +useClientReadyState().value );
 
+    console.log('bootRedirectRule |+++++++++++++++++++++++++++++++++++++++++++++++++');
     const routeTo = useRouteTo();
     const redirectRuleMap = redirectRuleMapState();
     const {isAuthenticated} = useAuth();
-    console.log('bootRedirectRule |+++++++++++++++++++++++++++++++++++++++++++++++++');
     console.log({'COMPOSABLE REDIRECT RULE IS AUTHENTICATED': isAuthenticated.value});
 
     const redirectRuleClosure = reactive({
+        'skipable-middleware' : {
+            'continue': true,//If continue is false, break the rule loop and perform rule callback
+            'callback': () => {
+                return console.log('COMPOSABLE RULE SKIPABLE MIDDLEWARE CALLBACK');
+            }
+        },
         'guest' : {
             'continue': !isAuthenticated.value,
             'callback': () => {
                 return navigateTo("/", {replace: true});
-            }
-        },
-        'sample-middleware' : {
-            'continue': true,
-            'callback': () => {
-                return console.log('COMPOSABLE RULE SAMPLE MIDDLEWARE CALLBACK');
             }
         }
     });
@@ -69,10 +66,8 @@ export const bootRedirectRule = async function(rules = []){
 
         ruleCallback();
 
-        setTimeout(()=>{
-            console.log('bootRedirectRule with callback ClientReadyState = true');
-            useClientReadyState().value = true;
-        }, 300);
+        console.log('bootRedirectRule with callback ClientReadyState = true');
+        useClientReadyState().value = true;
 
     } else {
         console.log('bootRedirectRule without callback ClientReadyState = true');
