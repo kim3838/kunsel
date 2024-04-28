b6f42d2602bfea8dd0b9e431643921216cd1e639
Kim@PC <kimdeguzman20@yahoo.com>
2024-04-28 18:45:15 +0800

    Middlewares: Update Route To State On Redirection

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,10 +1,16 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = userState();
 
+    console.log({'GUEST MIDDLEWARE USEROUTE TO': useRouteTo().value.name});
+
     console.log({'AUTH MIDDLEWARE TO PATH': to.path});
     console.log({'AUTH MIDDLEWARE TO NAME': to.name.toLowerCase()});
 
     if(!user.value && to.path !== '/login'){
+        useRouteTo().value = {
+            'name' : 'login'
+        };
+
         return navigateTo("/login", {replace: true});
     }
 })
--- middleware/guest.ts
+++ middleware/guest.ts
@@ -1,7 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = userState();
 
+    console.log({'GUEST MIDDLEWARE USEROUTE TO': useRouteTo().value.name});
+
+    console.log({'GUEST MIDDLEWARE TO PATH': to.path});
+    console.log({'GUEST MIDDLEWARE TO NAME': to.name.toLowerCase()});
+
     if(user.value && to.path !== '/'){
+
+        useRouteTo().value = {
+            'name' : 'index'
+        };
+
         return navigateTo("/", {replace: true});
     }
 })
