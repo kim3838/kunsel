a3bd2ce3a69caa6673e37641752104adc33518be
Kim@PC <kimdeguzman20@yahoo.com>
2024-05-13 16:43:16 +0800

    Remove Delay Test

--- app.vue
+++ app.vue
@@ -17,12 +17,6 @@
 //If authenticated is fetched via node with ssr, authenticated is remembered
 //On client first load, we refresh XSRF-TOKEN
 if(useAuth().isAuthenticated.value && process.client){
-    await new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve(1);
-        }, 2000);
-    });
-    console.log('GET /sanctum/csrf-cookie');
     await csrFetch("/sanctum/csrf-cookie");
 }
 
