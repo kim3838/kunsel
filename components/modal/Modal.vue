745b69ee6c00ec054d13cf76c58d9305bdac3c78
Kim@PC <kimdeguzman20@yahoo.com>
2024-05-18 11:29:09 +0800

    Modal Component Minor Changes
    - Remove transition
    - Add thread color border
    - Change drop shadow color from lining color to thread color

--- components/modal/Modal.vue
+++ components/modal/Modal.vue
@@ -1,17 +1,15 @@
 <template>
-    <transition name="fade">
-        <div v-show="show" class="tw-fixed tw-inset-0 tw-overflow-y-auto tw-px-0 tw-z-50">
-            <div v-show="show" class="tw-fixed tw-inset-0" @click="close">
-                <div class="tw-absolute tw-inset-0 modal-layer tw-opacity-75"></div>
-            </div>
+    <div v-show="show" class="tw-fixed tw-inset-0 tw-overflow-y-auto tw-px-0 tw-z-50">
+        <div v-show="show" class="tw-fixed tw-inset-0" @click="close">
+            <div class="tw-absolute tw-inset-0 modal-layer tw-opacity-75"></div>
+        </div>
 
-            <div v-show="show" class="tw-h-full tw-flex tw-items-center thread-border">
-                <div  class="modal-body tw-rounded-sm tw-overflow-hidden tw-w-full sm:tw-w-full sm:tw-mx-auto" :class="maxWidthClass">
-                    <slot></slot>
-                </div>
+        <div v-show="show" class="tw-h-full tw-flex tw-items-center thread-border">
+            <div  class="modal-body tw-rounded-sm tw-overflow-hidden tw-w-full sm:tw-w-full sm:tw-mx-auto" :class="maxWidthClass">
+                <slot></slot>
             </div>
         </div>
-    </transition>
+    </div>
 </template>
 
 <script>
@@ -25,14 +23,14 @@ export default {
             primary: primaryColor,
             neutral: neutralColor,
             body: bodyColor,
-            lining: liningColor,
+            thread: threadColor,
         } = storeToRefs($themeStore);
 
         return {
             primaryColor,
             neutralColor,
             bodyColor,
-            liningColor
+            threadColor
         };
     },
     props: {
@@ -76,24 +74,15 @@ export default {
 }
 </script>
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-    opacity: 0;
-}
-
 .modal-layer{
     background-color: transparent;
 }
 
 .modal-body{
     background-color: v-bind(bodyColor);
+    border: 1px solid v-bind(threadColor);
     filter:
-        drop-shadow(v-bind(liningColor) 0px 0px 100px)
-        drop-shadow(v-bind(liningColor) 0px 0px 50px);
+        drop-shadow(v-bind(threadColor) 0px 0px 100px)
+        drop-shadow(v-bind(threadColor) 0px 0px 50px);
 }
 </style>
