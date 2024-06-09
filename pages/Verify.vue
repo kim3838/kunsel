9769097545ac37ba762e88683dae36b542b2b69e
Kim@PC <kimdeguzman20@yahoo.com>
2024-06-09 11:00:12 +0800

    Email Verification Update CSR Fetch Usage and Wrapper Layout

--- pages/Verify.vue
+++ pages/Verify.vue
@@ -1,73 +1,68 @@
 <template>
-    <div class="tw-mx-auto tw-px-4 tw-max-w-screen-2xl tw-flex tw-justify-center">
-        <div class="tw-hidden sm:tw-block tw-px-4 tw-py-8 tw-h-96 tw-w-96">
-            <div class="tw-w-full tw-h-full tw-bg-contain tw-bg-center tw-bg-no-repeat" :style="{'background-image': 'url(/deco/undraw/undraw_secure_login_pdn4.svg)'}"></div>
-        </div>
+    <div>
+        <DefaultWrapper>
+            <div class="tw-mx-auto tw-px-4 tw-max-w-screen-2xl tw-flex tw-justify-center">
+                <AccentFrame class="tw-my-4">
+                    <template #content>
+                        <div class="tw-relative tw-py-4">
+                            <form @submit.prevent="execute" class="tw-w-80">
+                                <p class="tw-text-lg tw-font-semibold tw-mb-4">
+                                    Verify your email.
+                                </p>
 
-        <AccentFrame class="tw-my-4">
-            <template #content>
-                <div class="tw-relative tw-py-4">
-                    <form @submit.prevent="execute" class="tw-w-72">
-                        <p class="tw-text-lg tw-font-semibold tw-mb-4">
-                            Verify your email.
-                        </p>
-                        <p class="tw-text-base">
-                            Please click the verification button sent to your email address.
-                        </p>
-                        <p class="tw-text-sm tw-mt-4">
-                            Use the button below to resend the verification.
-                            Dont forget to check your email spam category.
-                        </p>
+                                <p class="tw-text-base">
+                                    Please click the verification button sent to your email address.
+                                </p>
+                                <p class="tw-text-sm tw-mt-4">
+                                    Use the button below to resend the verification.
+                                    Dont forget to check your email spam category.
+                                </p>
 
-                        <div class="tw-flex tw-mt-4 tw-items-center tw-justify-end">
-                            <Button
-                                :disabled="pending"
-                                :size="'md'"
-                                :variant="'flat'"
-                                :icon="pending ? 'eos-icons:installing' : 'ic:round-mail-outline'"
-                                :label="pending ? 'Requesting...' : 'Send Email Verification'"></Button>
+                                <div class="tw-flex tw-mt-4 tw-items-center tw-justify-end">
+                                    <Button
+                                        :disabled="pending"
+                                        :size="'md'"
+                                        :variant="'default'"
+                                        :icon="pending ? 'eos-icons:installing' : 'ic:round-mail-outline'"
+                                        :label="pending ? 'Requesting...' : 'Send Email Verification'"></Button>
+                                </div>
+                            </form>
                         </div>
-                    </form>
-                </div>
-            </template>
-        </AccentFrame>
+                    </template>
+                </AccentFrame>
+            </div>
+        </DefaultWrapper>
     </div>
 </template>
 
 <script setup>
 const {$coreStore} = useNuxtApp();
-
-definePageMeta({
-    middleware: [
-        'auth',
-        'not-verified'
-    ]
-});
-
+definePageMeta({middleware: ['auth', 'not-verified']});
+useLayout().setNavigationMode('solid', 'Verify.vue');
 let pending = ref(false);
 
-const {execute} = csrFetch("/api/email/verification-notification", {
-    method: 'POST',
-    immediate: false
-}, {
-    onRequest: () => {
-        pending.value = true;
-    },
-    onRequestError: () => {
-        pending.value = false;
-    },
-    onResponse: () => {
-        pending.value = false;
-    },
-    onSuccessResponse: (request, response, options) => {
-        $coreStore.setPrompt({
-            icon: 'ic:outline-mark-email-read',
-            title: 'Email Verification',
-            message: _get(response, '_data.message', ''),
-            action: {
-                label: 'Close'
-            }
-        });
-    }
-});
+const execute = async () => {
+    pending.value = true;
+
+    await csrFetch("/api/email/verification-notification", {
+        method: 'POST',
+    }, {
+        onRequestError: () => {
+            pending.value = false;
+        },
+        onResponse: () => {
+            pending.value = false;
+        },
+        onSuccessResponse: (request, response, options) => {
+            $coreStore.setPrompt({
+                icon: 'ic:outline-mark-email-read',
+                title: 'Email Verification',
+                message: _get(response, '_data.message', ''),
+                action: {
+                    label: 'Close'
+                }
+            });
+        }
+    });
+}
 </script>
