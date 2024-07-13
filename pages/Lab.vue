03cc9e6345354d40ccdad7a6716f782b3c1d9036
Kim@PC <kimdeguzman20@yahoo.com>
2024-07-13 10:19:42 +0800

    Lab Update
    - Horizontal rule inline css
    - Data from @/public/data/payload.js

--- pages/Lab.vue
+++ pages/Lab.vue
@@ -2,7 +2,7 @@
     <div>
         <LandingWrapper>
             <div class="tw-mx-auto tw-max-w-screen-2xl tw-pt-4">
-                <div v-if="true" class="tw-flex tw-mb-4">
+                <div v-if="false" class="tw-flex tw-mb-4">
                     <SampleEmitter
                         v-model:[dynamicFoo].capitalizeEveryWord ="fooComputed"
                         v-model:[dynamicBarCaller].capitalize="barComputed"
@@ -40,7 +40,7 @@
                     </HexagonFrame>
                 </div>
 
-                <div v-if="false" class="tw-grid tw-gap-4 tw-grid-cols-1 md:tw-grid-cols-2 xl:tw-grid-cols-3">
+                <div v-if="true" class="tw-grid tw-gap-4 tw-grid-cols-1 md:tw-grid-cols-2 xl:tw-grid-cols-3">
                     <div
                         v-for="item in leftToRightItems"
                         class="tw-h-[230px]">
@@ -79,7 +79,7 @@
                                                 :label="item.title"/>
                                         </div>
 
-                                        <div class="horizontal-rule"></div>
+                                        <div style="height: 1px;width: 100%;background: linear-gradient(to right, transparent 0%, #e3e3e3 30%, #e3e3e3 70%, transparent 100%);"></div>
 
                                         <div v-if="item?.bullets?.value?.length" class="tw-flex tw-flex-row tw-flex-nowrap">
                                             <div class="tw-w-full" :style="{'columns': (item.bullets.column ? item.bullets.column : 1)}">
@@ -110,7 +110,7 @@
                     </div>
                 </div>
 
-                <div v-if="false" class="tw-mt-6 tw-mx-auto tw-w-11/12">
+                <div v-if="true" class="tw-mt-6 tw-mx-auto tw-w-11/12">
                     <div class="tw-grid tw-gap-[1rem] tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-3 xl:tw-grid-cols-4">
                         <div
                             v-for="item in topToBottomItems" class="tw-h-[430px]">
@@ -149,7 +149,7 @@
                                                     :label="item.title"/>
                                             </div>
 
-                                            <div class="horizontal-rule"></div>
+                                            <div style="height: 1px;width: 100%;background: linear-gradient(to right, transparent 0%, #e3e3e3 30%, #e3e3e3 70%, transparent 100%);"></div>
 
                                             <div v-if="item?.bullets?.value?.length" class="tw-flex tw-flex-row tw-flex-nowrap">
                                                 <div class="tw-w-full" :style="{'columns': (item.bullets.column ? item.bullets.column : 1)}">
@@ -186,6 +186,7 @@
 </template>
 
 <script setup lang="ts">
+import dataPayload from '@/public/data/payload.js';
 definePageMeta({middleware: 'guest'});
 useLayout().setNavigationMode('solid', 'Lab.vue');
 
@@ -218,193 +219,11 @@ watch(barProp, value => {
     console.log({'BAR UPDATED' : value});
 });
 
-const leftToRightItems = ref([
-    {
-        'image':{
-            'path': '/images/product/202306201506003693.webp'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'Segotep Memphis-S Meow PC Case (M-ATX / ITX supported)',
-        'description': 'Designed in collaboration with Segotep, the MEOW Series chassis is a micro-ATX tower featuring panel art inspired by Bobi the orange tabby cat. The chassis features a semi-mesh front and side panels to allow airflow and improve cooling. The tempered glass side panel provides a clear view of the interior and the rest of the MEOW ORANGE Series components.',
-        'bullets': {
-            'value': [
-                'COLORFIRE Series',
-                'Supports mini-ITX and micro-ATX motherboards',
-                'Up to three 2.5" SSDs and one 3.5" HDD',
-                'Up to 240mm radiators of AIO liquid coolers Supports up to 170mm tall CPU coolers',
-                'Up to six 120mm case fans'
-            ]
-        },
-        'link': '/prototype',
-        'frameBorder': '#dedede',
-        'contentBorder': '#b7b7b7'
-    },
-    {
-        'image':{
-            'path': '/images/product/c76383ac-e6c0-4725-bec0-b171449c5960.webp'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'COLORFIRE B760M-MEOW WIFI D5 ORANGE',
-        'description': 'COLORFUL introduces the CVN B760M FROZEN WIFI D5 and CVN B760I FROZEN WIFI motherboards for gamers and enthusiasts.',
-        'bullets': {
-            'value': [
-                'COLORFIRE Series',
-                'Support the 14th/13th and 12th generation Intel adapting to the LGA 1700 slot of the motherboard ® Core TM, Pentium ® And Cylon ® processor',
-                'Support dual channel DDR5-7200 (OC) MHz memory',
-                'Provide 3 PCIe 4.0 x4 M.2 SSD slots',
-                'Provide one USB 3.2 Gen 2 Type-C and two USB 3.2 Gen 1 Type-A high-speed rear interfaces'
-            ]
-        },
-        'link': '/prototype',
-        'frameBorder': '#dedede',
-        'contentBorder': '#b7b7b7'
-    },
-    {
-        'image':{
-            'path': '/images/product/d942910a-ffe9-461c-8847-a6fdb4ab480c.webp'
-        },
-        'listIcon': 'bi:gpu-card',
-        'title': 'COLORFIRE GeForce RTX 4070 SUPER MEOW-ORG 12GB-V',
-        'description': '',
-        'bullets': {
-            'value': [
-                'COLORFIRE Series',
-                'Efficient and durable heat dissipation.',
-                'With triple fans to enhance air pressure and air input, supporting auto-stop technology.',
-                'Adjustable lighting effects via iGame Center.'
-            ]
-        },
-        'link': '/prototype',
-        'frameBorder': '#dedede',
-        'contentBorder': '#b7b7b7'
-    },
-]);
-const topToBottomItems = ref([
-    {
-        'image':{
-            'path': '/images/spotlight/5037c274-2024-46d3-8f04-e70ccea4ec8f.webp'
-        },
-        'listIcon': 'bi:gpu-card',
-        'title': 'COLORFUL Introduces GeForce RTX 4070 Series Graphics Card Lineup',
-        'link': '/prototype',
-        'description': '',
-        'bullets': {
-            'value': [
-                'iGame GeForce RTX 4070 Vulcan OC',
-                'COLORFUL iGame GeForce RTX 4070 Neptune OC',
-                'COLORFUL iGame GeForce RTX 4070 Advanced OC',
-                'COLORFUL iGame GeForce RTX 4070 Ultra W',
-                'COLORFUL GeForce RTX 4070 NB EX'
-            ]
-        },
-        'frameBorder': '#C2446F',
-        'contentBorder': '#922042'
-    },
-    {
-        'image':{
-            'path': '/images/spotlight/w636.jpg'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'ASUS B550 Series- Rise Above the Battlefield',
-        'link': '/prototype',
-        'description': 'ASUS B550 Series packs all the essentials for the latest AMD processors. WiFi 6 and 2.5 Gb Ethernet provide ample bandwidth for online gaming, while AI Noise-Canceling Mic software ensures you’re clearly heard in-game. And system set up is a breeze with the intuitive BIOS Flashback™.',
-        'frameBorder': '#f9dbbd',
-        'contentBorder': '#f4a261'
-    },
-    {
-        'image':{
-            'path': '/images/spotlight/w637.jpg'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'ASUS Z490 Series- AI Motherboards',
-        'link': '/prototype',
-        'description': 'ASUS Z490 motherboards feature intelligent overclocking, revolutionary cooling, a robust power solution, optimized memory and next-gen connectivity to unleash the power of the latest 10th Gen Intel® Core™ processors.',
-        'frameBorder': '#a8dadc',
-        'contentBorder': '#4b6e7a'
-    },
-    {
-        'image':{
-            'path': '/images/spotlight/c869bdd2-4aa7-408d-b2af-75681836aa9a.webp'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'COLORFUL Launches EVOL P15 Gaming Laptop',
-        'link': '/prototype',
-        'description': null,
-        'bullets': {
-            'column': 2,
-            'value': [
-                'Retro-Futuristic Design',
-                'Deep Chiller System',
-            ]
-        },
-        'frameBorder': '#a8dadc',
-        'contentBorder': '#4b6e7a'
-    },
-    {
-        'image':{
-            'path': '/images/spotlight/CODM-S4-FOOLS-GOLD-009.jpg'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'FOOL\'S GOLD',
-        'link': '/prototype',
-        'description': 'Season 4 — Fool’s Gold launches on Call of Duty: Mobile on April 17 at 5 PM PT.',
-        'frameBorder': '#3b6b2a',
-        'contentBorder': '#3b6b2a'
-    },
-    {
-        'image':{
-            'path': '/images/spotlight/1697501586302.jpg'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'New Z790 motherboards from ROG pave the way for 14th Gen Intel Core CPUs',
-        'link': '/prototype',
-        'description': 'WiFi 7 support, more fast storage, front-panel device charging, intelligent controls — our latest Z790 motherboards have it all.',
-        'frameBorder': '#a8dadc',
-        'contentBorder': '#4b6e7a'
-    },
-    {
-        'image':{
-            'path': '/images/spotlight/w638.jpg'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'ROG Gaming Audio',
-        'link': '/prototype',
-        'description': 'Utilizing advanced trace layout techniques to mitigate noise, and featuring carefully selected components and a host of software options that enhance the gaming experience, ROG Gaming Audio pushes onboard audio to the very limits.',
-        'frameBorder': '#696969',
-        'contentBorder': '#457b9d'
-    },
-    {
-        'image':{
-            'path': '/images/spotlight/1692323352671.webp'
-        },
-        'listIcon': 'octicon:circuit-board',
-        'title': 'Race after your dreams with the power of the ROG Strix SCAR 16 and SCAR 18',
-        'link': '/prototype',
-        'description': 'Discover the amazing true story of a gamer turned pro racer in the new movie Gran Turismo.',
-        'frameBorder': '#f9dbbd',
-        'contentBorder': '#f4a261'
-    },
-]);
-
-
-onMounted(async () => {
-    nextTick(() => {
-
-    });
-});
+const leftToRightItems = ref(dataPayload['leftToRightItems']);
+const topToBottomItems = ref(dataPayload['topToBottomItems']);
 
 </script>
 
 <style scoped lang="scss">
-.horizontal-rule{
-    height: 1px;
-    width: 100%;
-    background: linear-gradient(
-        to right,
-        transparent 0%,
-        #e3e3e3 30%,
-        #e3e3e3 70%,
-        transparent 100%
-    );
-}
+
 </style>
