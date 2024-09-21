09bc2b124073e84bad67a26b2811d761893d5fb3
Kim@PC <kimdeguzman20@yahoo.com>
2024-09-21 11:52:54 +0800

    Refactor Auth Plugin

--- plugins/06.auth.ts
+++ plugins/06.auth.ts
@@ -2,11 +2,22 @@ export default defineNuxtPlugin({
     enforce: 'pre',
 
     async setup(nuxtApp){
-        const {user, fetchUser} = useAuth();
+        const {user} = useAuth();
 
-        if (user.value !== undefined || process.client) return;
+        const userAuthenticatedOrDuringClientSideRendering: Boolean = (user.value !== undefined || process.client);
 
-        await fetchUser();
+        if (userAuthenticatedOrDuringClientSideRendering) return;
+
+        //Fetch user
+        const {baseURL} = useRuntimeConfig().public;
+
+        const {data: userData} = await useFetch(baseURL + "/api/user", {
+            credentials: 'include',
+            headers: laraHeaders(),
+            async onResponse({ request, response, options }) {}
+        });
+
+        user.value = _get(userData.value, 'values', undefined);
     },
 
     hooks: {
