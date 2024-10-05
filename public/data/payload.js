1c2d9a8f4d762aef7f3f7fcb6d96a982245080e9
Kim@PC <kimdeguzman20@yahoo.com>
2024-10-05 11:29:01 +0800

    Payload Data: Sample Carousel Items and FAQs

--- public/data/payload.js
+++ public/data/payload.js
@@ -726,5 +726,214 @@ export default {
                 ]
             },
         }
-    }
+    },
+    'carousel': {
+        'newArrivals': [
+            {
+                'image': '/images/hero/21f0a52e-db0a-4108-9405-44a8a548e534.webp',
+                'title': 'MEOW SET',
+                'subTitle': 'RTX 4060 Ti MEOW-ORG OC 16GB / B760M-MEOW WIFI D5 ORANGE.',
+                'link': '/'
+            },
+            {
+                'image': '/images/hero/ea05cc1d-1384-47f4-84ab-b3c049f7e13e.webp',
+                'title': 'AIO PC',
+                'subTitle': 'iGame G-ONE Plus i7-12700H/RTX3060.',
+                'link': '/'
+            },
+            {
+                'image': '/images/hero/353f5cb4-8113-43fa-b5c4-3a9bf1314474.webp',
+                'title': 'Laptop',
+                'subTitle': 'Colorful EVOL P15 ',
+                'link': '/'
+            },
+            {
+                'image': '/images/hero/667a2407-ae78-4e4d-b69e-059552bca96e.webp',
+                'title': 'Graphics Card',
+                'subTitle': 'iGame GeForce RTX 4090 Vulcan OC-V',
+                'link': '/'
+            },
+            {
+                'image': '/images/hero/a5a3aa00-67ea-4c3e-b6b4-6b9b92a0a50a.webp',
+                'title': 'Motherboard',
+                'subTitle': 'CVN B760I FROZEN WIFI D5 V20',
+                'link': '/'
+            },
+            {
+                'image': '/images/hero/607742af-81ec-4893-b94c-1b49c228fbde.webp',
+                'title': 'SSD',
+                'subTitle': 'CN700 1TB',
+                'link': '/'
+            }
+        ],
+        'featuredItems': [
+            {
+                'image': '/images/product/f85a85cd-fa65-48d3-9d36-755b4a6acf87.webp',
+                'title': 'Graphics Card',
+                'subTitle': 'COLORFIRE GeForce RTX 4060 Ti MEOW-ORG 8GB-V',
+                'link': '/prototype'
+            },
+            {
+                'image': '/images/product/176a61f6-884b-4def-9c35-5d30cdca686f.webp',
+                'title': 'Graphics Card',
+                'subTitle': 'iGame GeForce RTX 4080 16GB Ultra W OC-V',
+                'link': '/prototype'
+            },
+            {
+                'image': '/images/product/cd8f248a-bd3d-4553-bcf7-849ed27c4b36.webp',
+                'title': 'Graphics Card',
+                'subTitle': 'iGame GeForce RTX 4070 Ti Ultra W OC-V',
+                'link': '/prototype'
+            },
+            {
+                'image': '/images/product/20220923155420669106.webp',
+                'title': 'Graphics Card',
+                'subTitle': 'iGame GeForce RTX 4090 Vulcan OC-V',
+                'link': '/prototype'
+            }
+        ],
+        'trendingItems': [
+            {
+                'image': '/images/product/c7dfc1f1-102a-4ea9-84f6-f87dda2094b8.webp',
+                'title': 'Motherboard',
+                'subTitle': 'CVN Z790D5 GAMING PRO WIFI V20',
+                'link': '/prototype'
+            },
+            {
+                'image': '/images/product/38131462-2ae0-443e-8555-9e744c532887.webp',
+                'title': 'Motherboard',
+                'subTitle': 'BATTLE-AX Z790AK-PLUS D5 V20',
+                'link': '/prototype'
+            },
+            {
+                'image': '/images/product/ebac5037-c3d2-4275-b96b-24f855695841.webp',
+                'title': 'Motherboard',
+                'subTitle': 'iGame Z790D5 ULTRA V20',
+                'link': '/prototype'
+            },
+            {
+                'image': '/images/product/f9b5a1a7-d532-4cf8-970c-d812b857a666.webp',
+                'title': 'Motherboard',
+                'subTitle': 'iGame Z790D5 FLOW V20',
+                'link': '/prototype'
+            }
+        ]
+    },
+    'faqs': {
+        'options': [
+            {
+                title: 'Coldiron Armor Set',
+                body: {
+                    type: 'text',
+                    value: 'This armor was forged for the sole purpose of resisting the malignance spreading across the land.'
+                },
+                active: true
+            },
+            {
+                title: 'Coldiron Barding Mount Armor',
+                body: {
+                    type: 'text',
+                    value: 'Adorn your new steeds with mount armor inspired by the malignance taking over Sanctuary.'
+                },
+                active: false
+            },
+            {
+                title: 'Weapon skins, Mount Trophies, Emotes, Platinum, and more - Weapon skins, Mount Trophies, Emotes, Platinum, and more',
+                body: {
+                    type: 'text',
+                    value: 'Explore a range of cosmetics to earn along the Premium Battle Pass. Collect a weapon transmog for all 19 weapon types. 2 emotes for each class let you threaten your enemies or find a little time to play around with malignant heart cages. 2 headstones even let you die in style. Adorn your horse with 5 new mount trophies.'
+                },
+                active: false
+            },
+            {
+                title: '“On the Warpath” Emote – Accelerated Battle Pass',
+                body: {
+                    type: 'text',
+                    value: 'Show everyone you’re ready for battle when you slam your banner down, signaling to enemies that you’re ready for a fight. This emote is only included in the Accelerated Battle Pass.'
+                },
+                active: false
+            }
+        ]
+    },
+    'news': [
+        {
+            'image':{
+                'path': '/images/spotlight/CODM-S4-FOOLS-GOLD-009.jpg'
+            },
+            'title': 'COLORFUL Launches The GeForce RTX 4060 Ti and RTX 4060 Series Graphics Cards Supercharged by DLSS 3',
+            'subTitle': 'COLORFUL introduces the CVN B760M FROZEN WIFI D5 and CVN B760I FROZEN WIFI motherboards for gamers and enthusiasts. Coming in a compact micro-ATX and mini-ITX form factors, both motherboards feature PCIe 5.0 x16 slot to support the latest high-end graphics cards. The CVN B760 motherboards also come with Wi-Fi 6, three PCIe 4.0 M.2 slots for the CVN B760M FROZEN WIFI D5 and two PCIe 4.0 M.2 slots for the CVN B760I FROZEN WIFI mini-ITX motherboard. COLORFUL also presents the BATTLE-AX B760M-F PRO motherboard with DDR4 memory support and two PCIe 4.0 M.2 slots.',
+        },
+        {
+            'image':{
+                'path': '/images/spotlight/CODM-S4-FOOLS-GOLD-002.jpg'
+            },
+            'title': 'Seize the Season With RTX',
+            'subTitle': 'Celebrate the holiday season with the ultimate upgrade.Treat yourself to unrivaled performance and features with GeForce RTX™ 40 Series graphics cards and laptops built to transform every experience.',
+        },
+        {
+            'image':{
+                'path': '/images/spotlight/CODM-S4-FOOLS-GOLD-TOUT.jpg'
+            },
+            'title': 'COLORFUL Launches B760 Series Motherboards',
+            'subTitle': 'COLORFUL introduces the CVN B760M FROZEN WIFI D5 and CVN B760I FROZEN WIFI motherboards for gamers and enthusiasts. Coming in a compact micro-ATX and mini-ITX form factors, both motherboards feature PCIe 5.0 x16 slot to support the latest high-end graphics cards. The CVN B760 motherboards also come with Wi-Fi 6, three PCIe 4.0 M.2 slots for the CVN B760M FROZEN WIFI D5 and two PCIe 4.0 M.2 slots for the CVN B760I FROZEN WIFI mini-ITX motherboard. COLORFUL also presents the BATTLE-AX B760M-F PRO motherboard with DDR4 memory support and two PCIe 4.0 M.2 slots.',
+        },
+        {
+            'image':{
+                'path': '/images/spotlight/CODM-S4-FOOLS-GOLD-003.jpg'
+            },
+            'title': 'Further With AI, Faster on RTX',
+            'subTitle': 'From enhanced creativity + ultra-efficient productivity to blisteringly fast gaming. The ultimate in AI power on Windows PCs is on NVIDIA RTX and GeForce RTX.',
+        },
+        {
+            'image':{
+                'path': '/images/spotlight/CODM-S4-FOOLS-GOLD-001.jpg'
+            },
+            'title': 'PC Game Pass Game Bundle',
+            'subTitle': 'Get PC Game Pass for 3 months with select GeForce RTX 40 Series*',
+        },
+        {
+            'image':{
+                'path': '/images/spotlight/CODM-S4-FOOLS-GOLD-008.jpg'
+            },
+            'title': 'COLORFUL Launches EVOL X15 AT Gaming Laptop Powered by Intel 13th Gen CPUs and NVIDIA GeForce RTX 4060 GPU and NVIDIA GeForce RTX 4060 GPU and NVIDIA GeForce RTX 4060 GPU',
+            'subTitle': 'The EVOL X15 AT sports a 15.6” QHD (2560x1440) 165Hz display with NVIDIA G-SYNC support for stutter-free and tear-free gaming experience. Aside from Pine Blue and Mist Grey, the new EVOL X15 AT gaming laptop comes in the new Purple Grape color.',
+        },
+    ],
+    'featured': [
+        {
+            'type': 'full',
+            'title': 'Featured',
+            'image': '/images/carousel/c12d1eba-e8c2-4827-a0d2-5298c4b0f421.webp'
+        },
+        {
+            'type': 'full',
+            'title': 'Top Seller',
+            'image': '/images/carousel/c12d1eba-e8c2-4827-a0d2-5298c4b0f421.webp'
+        },
+        {
+            'type': 'full',
+            'title': 'Trending',
+            'image': '/images/carousel/c12d1eba-e8c2-4827-a0d2-5298c4b0f421.webp'
+        },
+        {
+            'type': 'full',
+            'title': 'Battle Pass',
+            'image': '/images/carousel/c12d1eba-e8c2-4827-a0d2-5298c4b0f421.webp'
+        },
+        // {
+        //     'type': 'full',
+        //     'title': 'FEATURED',
+        //     'image': '/images/carousel/79e97448-7388-4f71-876e-d33c92091910.webp'
+        // },
+        // {
+        //     'type': 'full',
+        //     'title': 'TOP SELLER',
+        //     'image': '/images/carousel/202ae924-b777-45a2-94c6-f78134b17020.webp'
+        // },
+        // {
+        //     'type': 'full',
+        //     'title': 'TRENDING',
+        //     'image': '/images/carousel/2271fbba-86bf-4dc7-93cb-59eb0aab62d8.webp'
+        // },
+    ]
 };
