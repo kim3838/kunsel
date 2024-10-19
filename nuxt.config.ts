8714f24641089fea88e10d5e19e945f0208ae3fd
Kim@PC <kimdeguzman20@yahoo.com>
2024-10-19 14:15:56 +0800

    Nuxt Config: Fix Whitespaces

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,57 +2,57 @@
 
 // @ts-ignore
 export default defineNuxtConfig({
-  app: {
-      baseURL: "/",
-      head: {
-          title: 'sn-f v3',
-          "meta": [
-              {"name": "viewport","content": "width=device-width, initial-scale=1"},
-              {"charset": "utf-8"}
-          ],
-          "link": [
-              {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
-              {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'use-credentials'},
-              //Sans-Serif
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Sofia+Sans+Semi+Condensed:ital,wght@0,1;0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,1;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital@0;1&display=swap" rel="stylesheet'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Hedvig+Letters+Sans&display=swap" rel="stylesheet'},
-              //Serif
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Gupter:wght@400;500;700&family=Kotta+One&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oranienbaum&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&display=swap'},
-              //Monospace
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap'},
-              {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Spline+Sans+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'}
-          ],
-          "style": [],
-          "script": [
-              {src: '/js/common/moment.min.js'},
-              {src: '/js/common/jquery.min.js'},
-              {src: '/js/datetimepicker/index.js'}
-          ],
-          "noscript": [],
-          "htmlAttrs": {
-              class: ''
-          },
-          "bodyAttrs": {
-              class: 'tw-font-paragraph'
-          }
-      },
-      layoutTransition: false,
-      pageTransition: false,
-      rootId: "__nuxt",
-      rootTag: "div"
-  },
+    app: {
+        baseURL: "/",
+        head: {
+            title: 'sn-f v3',
+            "meta": [
+                {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+                {"charset": "utf-8"}
+            ],
+            "link": [
+                {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+                {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'use-credentials'},
+                //Sans-Serif
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Sofia+Sans+Semi+Condensed:ital,wght@0,1;0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,1;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital@0;1&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Hedvig+Letters+Sans&display=swap" rel="stylesheet'},
+                //Serif
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Gupter:wght@400;500;700&family=Kotta+One&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oranienbaum&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&display=swap'},
+                //Monospace
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Spline+Sans+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'}
+            ],
+            "style": [],
+            "script": [
+                {src: '/js/common/moment.min.js'},
+                {src: '/js/common/jquery.min.js'},
+                {src: '/js/datetimepicker/index.js'}
+            ],
+            "noscript": [],
+            "htmlAttrs": {
+                class: ''
+            },
+            "bodyAttrs": {
+                class: 'tw-font-paragraph'
+            }
+        },
+        layoutTransition: false,
+        pageTransition: false,
+        rootId: "__nuxt",
+        rootTag: "div"
+    },
 
-  //prints out hook names and timings on the server, and logs hook arguments as well in the browser.
-  debug: false,
+    //prints out hook names and timings on the server, and logs hook arguments as well in the browser.
+    debug: false,
 
     imports: {
         dirs: [
@@ -114,7 +114,7 @@ export default defineNuxtConfig({
     },
 
     tailwindcss: {
-        cssPath: ['@/assets/css/tailwind.css', { injectPosition: "first" }],
+        cssPath: ['@/assets/css/tailwind.css', {injectPosition: "first"}],
         viewer: true
     },
 
@@ -130,11 +130,11 @@ export default defineNuxtConfig({
         apiKey: '' // Default to an empty string, automatically set at runtime using process.env.NUXT_API_KEY
     },
 
-  devServer: {
-      host: 'client.server.local',
-      https: false,
-      port: 3000,
-  },
+    devServer: {
+        host: 'client.server.local',
+        https: false,
+        port: 3000,
+    },
 
-  compatibilityDate: '2024-08-30'
+    compatibilityDate: '2024-08-30'
 })
