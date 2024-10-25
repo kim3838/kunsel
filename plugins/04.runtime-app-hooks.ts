520391243cb5080eecfa66026dcb09a638372b80
Kim@PC <kimdeguzman20@yahoo.com>
2024-10-25 10:49:40 +0800

    Runtime App Hooks Update Debug Loggers

--- plugins/04.runtime-app-hooks.ts
+++ plugins/04.runtime-app-hooks.ts
@@ -6,39 +6,38 @@ export default defineNuxtPlugin((nuxtApp) => {
 
     //Called when the setup() of the new page is running.
     nuxtApp.hook('page:loading:start', () => {
-        console.log({'PAGE NAVIGATE': 'page:loading:start'});
+        //console.log('PAGE NAVIGATE: setup() of the new page is running');
     });
 
     //Called when initial vueApp instance is created.
     nuxtApp.hook('app:created', () => {
-        console.log({'RUNTIME APP HOOK': 'app:created'});
-        console.log('runtime app hook app:created ClientReadyState = false');
+        //console.log('RUNTIME APP HOOK: vueApp instance is created');
         useClientReadyState().value = false;
     });
 
     //Called when Vue app is initialized and mounted in browser.
     nuxtApp.hook('app:mounted', () => {
-        //console.log({'RUNTIME APP HOOK': 'app:mounted'});
+        //console.log('RUNTIME APP HOOK: vueApp mounted in browser');
     });
 
     //Called when SSR rendering is done.
     nuxtApp.hook('app:rendered', () => {
-        //console.log({'RUNTIME APP HOOK': 'app:rendered'});
+        //console.log('RUNTIME APP HOOK: SSR rendering is done');
     });
 
     //Called on Suspense pending event.
     nuxtApp.hook('page:start', () => {
-        //console.log({'PAGE NAVIGATE': 'page:start'});
+        //console.log('PAGE NAVIGATE: Suspense pending event');
     });
 
     //Called on Suspense resolved event.
     nuxtApp.hook('page:finish', () => {
-        //console.log({'PAGE NAVIGATE': 'page:finish'});
+        //console.log('PAGE NAVIGATE: Suspense resolved event');
     });
 
     //Called after page:finish
     nuxtApp.hook('page:loading:end', () => {
-        console.log({'PAGE NAVIGATE': 'page:loading:end'});
+        //console.log('PAGE NAVIGATE: page:finish');
     });
 })
 
