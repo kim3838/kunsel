e22958d99f976014a1c81b91541deb70efed71f9
Kim@PC <kimdeguzman20@yahoo.com>
2024-10-29 10:39:43 +0800

    Helper: toRomanNumeral

--- plugins/02.core.ts
+++ plugins/02.core.ts
@@ -16,6 +16,39 @@ export default defineNuxtPlugin(nuxtApp => {
         return number + (s[(v - 20) % 10] || s[v] || s[0]);
     });
 
+    nuxtApp.provide('toRomanNumeral', function (num) {
+
+        if(num == 0){
+            return 'i';
+        }
+
+        const romanNumerals = [
+            { value: 1000, symbol: "M" },
+            { value: 900, symbol: "CM" },
+            { value: 500, symbol: "D" },
+            { value: 400, symbol: "CD" },
+            { value: 100, symbol: "C" },
+            { value: 90, symbol: "XC" },
+            { value: 50, symbol: "L" },
+            { value: 40, symbol: "XL" },
+            { value: 10, symbol: "X" },
+            { value: 9, symbol: "IX" },
+            { value: 5, symbol: "V" },
+            { value: 4, symbol: "IV" },
+            { value: 1, symbol: "I" }
+        ];
+
+        let result = "";
+        for (let i = 0; i < romanNumerals.length; i++) {
+            const { value, symbol } = romanNumerals[i];
+            while (num >= value) {
+                result += symbol;
+                num -= value;
+            }
+        }
+        return result;
+    });
+
     nuxtApp.provide('isRouteActive', function (routeSlug: string) {
         return [route.path, _toLower(route.name)].indexOf(_toLower(routeSlug)) >= 0;
     });
