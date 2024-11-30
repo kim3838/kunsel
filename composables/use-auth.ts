96ebb9fa3ed2ab50595d633fdab9c593ed7dc182
Kim@PC <kimdeguzman20@yahoo.com>
2024-11-30 10:16:01 +0800

    Auth Home Page As Index

--- composables/use-auth.ts
+++ composables/use-auth.ts
@@ -77,7 +77,7 @@ export const useAuth = () => {
                 } else {
                     await fetchUser();
                     await navigateTo({
-                        path: '/featured',
+                        path: '/',
                         replace: true
                     });
                 }
