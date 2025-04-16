f0c12c68af2f7c2e1ea4fb520e30426780bdd6ae
Kim@PC <kimdeguzman20@yahoo.com>
2025-04-16 10:55:28 +0800

    Auth Store

---
+++ stores/auth.ts
@@ -0,0 +1,37 @@
+import {defineStore} from 'pinia'
+
+export const useAuthStore = defineStore('auth', () => {
+
+    const SELECTED_ASSOCIATED_COMPANY_STORAGE_KEY = 'persist_company';
+
+    const associatedCompanies = ref({
+        singleSelectPayload: {
+            search: '',
+            data: [],
+            selection: [],
+            selected: null
+        }
+    });
+
+    const selectedAssociatedCompany = computed(() => {
+        return associatedCompanies.value.singleSelectPayload.selected;
+    })
+
+    function resetAssociatedCompanies() {
+        associatedCompanies.value = {
+            singleSelectPayload: {
+                search: '',
+                data: [],
+                selection: [],
+                selected: null
+            }
+        }
+    }
+
+    return {
+        SELECTED_ASSOCIATED_COMPANY_STORAGE_KEY,
+        associatedCompanies,
+        resetAssociatedCompanies,
+        selectedAssociatedCompany
+    }
+})
