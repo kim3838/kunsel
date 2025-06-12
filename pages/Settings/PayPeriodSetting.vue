a56d1d4833feb01498dbf321c69b564d6b6d1914
Kim@PC <kimdeguzman20@yahoo.com>
2025-06-12 18:31:08 +0800

    Add Preset Selection and Submission for Pay Period Settings
    - Added `PayPeriodSettingT` and `PayPeriodPresetSelectionT` types for better type definitions.
    - Implemented preset selection UI in `PayPeriodSetting.vue` with `SingleSelect` and preset options.
    - Added APIs to fetch preset selections and update pay period settings.
    - Introduced `updatePayPeriods` function to handle preset changes dynamically.
    - Included `submit` function to save updated pay period settings to the server.

--- pages/Settings/PayPeriodSetting.vue
+++ pages/Settings/PayPeriodSetting.vue
@@ -6,6 +6,20 @@
                     <div>Days to pay after cut off</div>
                     <div>{{`${payPeriodSetting.days_to_pay_after_cut_off} days`}}</div>
                     <br>
+                    <InputLabel :size="'md'" value="Monthly and Semimonthly 2nd half Cut-off Presets" />
+                    <div class="flex">
+                        <SingleSelect
+                            :width="'220px'"
+                            :searchable="false"
+                            drop-shadow
+                            value-persist
+                            :selection-max-viewable-line="6"
+                            :size="'md'"
+                            :label="'Select Cut-off Presets'"
+                            :options="payPeriodPresetOptions"
+                            @value-change="updatePayPeriods"/>
+                    </div>
+                    <br>
                     <div>Monthly Pay Period</div>
                     <table class="border-separate text-sm font-sub-data">
                         <tbody>
@@ -23,6 +37,13 @@
                         </tr>
                         </tbody>
                     </table>
+                    <br>
+                    <Button
+                        class="max-w-min"
+                        :disabled="submitPending"
+                        :icon="submitPending ? 'eos-icons:loading' : 'mdi:data'"
+                        @click="submit"
+                        :label="'Update'"></Button>
                 </div>
                 <div v-else>
                     Pay period setting not found.
@@ -34,6 +55,7 @@
 
 <script setup lang="ts">
 import {storeToRefs} from "pinia";
+import type {PayPeriodPresetSelectionT, PayPeriodSettingT} from "@/public/js/types/company-component";
 
 definePageMeta({middleware: ['auth', 'company-admin']});
 useLayout().setNavigationMode('solid', 'PayPeriodSetting.vue');
@@ -52,12 +74,28 @@ watch(updatedAssociatedCompanyFlag, (newValue) => {
     }
 });
 
-const payPeriodSetting = ref({});
+const payPeriodPresetOptions = reactive<{
+    search: string,
+    data: PayPeriodPresetSelectionT[],
+    selection: PayPeriodPresetSelectionT[],
+    selected: number | string | null
+}>({
+    search: '',
+    data: [],
+    selection: [],
+    selected: null
+});
+
+const payPeriodSetting = ref<PayPeriodSettingT>({} as PayPeriodSettingT);
 
 const payPeriodSettingPending = ref(false)
 const payPeriodSettingExecute = async () => {
 
-    payPeriodSetting.value = {};
+    if(!selectedAssociatedCompany.value){
+        return;
+    }
+
+    payPeriodSetting.value = {} as PayPeriodSettingT;
     payPeriodSettingPending.value = true;
 
     await laraFetch(`/api/company-pay-period-setting/${selectedAssociatedCompany.value}`, {
@@ -70,14 +108,77 @@ const payPeriodSettingExecute = async () => {
             payPeriodSettingPending.value = false;
         },
         onSuccessResponse: async (request, options, response) => {
-            const payPeriodSettingResponse = _get(response, '_data.values.pay_period_setting', {});
-            payPeriodSetting.value = payPeriodSettingResponse
+            const payPeriodSettingResponse: PayPeriodSettingT = _get(response, '_data.values.pay_period_setting', {} as PayPeriodSettingT);
+            const timePeriodPreset = _get(response, '_data.values.pay_period_setting.time_period_preset_reference', null);
+
+            payPeriodSetting.value = payPeriodSettingResponse;
+            payPeriodPresetOptions.selected = timePeriodPreset;
         }
     }, false);
 }
 await payPeriodSettingExecute();
 
+const payPeriodPresetPending = ref(false)
+const payPeriodPresetExecute = async () => {
 
+    payPeriodPresetPending.value = true;
+
+    await laraFetch(`/api/pay-period-preset-selections`, {
+        method: 'GET',
+    }, {
+        onRequestError: () => {
+            payPeriodPresetPending.value = false;
+        },
+        onResponse: () => {
+            payPeriodPresetPending.value = false;
+        },
+        onSuccessResponse: async (request, options, response) => {
+            const payPeriodPresetSelection = _get(response, '_data.values.selection', []);
+            payPeriodPresetOptions.data = payPeriodPresetSelection;
+            payPeriodPresetOptions.selection = payPeriodPresetSelection;
+        }
+    }, false);
+}
+await payPeriodPresetExecute();
+
+const updatePayPeriods = () => {
+    const selectedPayPeriodPreset: PayPeriodPresetSelectionT | undefined = payPeriodPresetOptions.selection.find((payPeriodPresetOption) => {
+        return payPeriodPresetOption.value == String(payPeriodPresetOptions.selected);
+    });
+
+    if (!selectedPayPeriodPreset) {
+        return;
+    }
+
+    payPeriodSetting.value.time_period_preset_reference = selectedPayPeriodPreset.value;
+    payPeriodSetting.value.monthly_pay_period = selectedPayPeriodPreset.monthly_period;
+    payPeriodSetting.value.semimonthly_pay_period = selectedPayPeriodPreset.semimonthly_period;
+};
+
+const submitPending = ref(false);
+const submit = async() => {
+
+    if(!payPeriodSetting.value.id){
+        return;
+    }
+
+    submitPending.value = true;
+
+    await laraFetch(`/api/pay-period-setting/${payPeriodSetting.value.id}`, {
+        method: 'PATCH',
+        body: payPeriodSetting.value
+    }, {
+        onRequestError: () => {
+            submitPending.value = false;
+        },
+        onResponse: () => {
+            submitPending.value = false;
+        },
+        onSuccessResponse: async (request, options, response) => {
+
+        },
+    });
+}
 </script>
 
 <style scoped>
--- public/js/types/company-component.ts
+++ public/js/types/company-component.ts
@@ -8,3 +8,19 @@ export type CompanyFormulaT = {
     value: string | number| null,
     value_type: string,
 };
+
+export type PayPeriodSettingT = {
+    id: string | number,
+    company_id: string | number,
+    days_to_pay_after_cut_off: string | number,
+    time_period_preset_reference: string,
+    monthly_pay_period: string,
+    semimonthly_pay_period: string,
+}
+
+export type PayPeriodPresetSelectionT = {
+    value: string,
+    text: string | number,
+    monthly_period: string,
+    semimonthly_period: string,
+}
