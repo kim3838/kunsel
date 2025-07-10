2026d30c143868e9b2727bde7ca9733838d245ff
Kim@PC <kimdeguzman20@yahoo.com>
2025-07-11 07:28:56 +0800

    Update FancyFrame With Secondary Color Support
    - Added `secondaryColor` binding from theme store for gradient color flexibility.
    - Replaced `primaryColor` with `secondaryColor` in gradient backgrounds for `.top-fragment` and `.bottom-fragment`.
    - Adjusted SCSS styles to enhance theme-driven customization.

--- components/frame/FansyFrame.vue
+++ components/frame/FansyFrame.vue
@@ -44,6 +44,7 @@ const {
     lining: liningColor,
     shade: shadeColor,
     primary: primaryColor,
+    secondary: secondaryColor,
 } = storeToRefs($themeStore);
 
 const { height: contentHeight } = useElementSize(useTemplateRef('contentContainerRef'));
@@ -71,12 +72,12 @@ const frameContainerStyle = computed(() => {
 </script>
 <style lang="scss">
 .top-fragment{
-    background-image: linear-gradient(to right top, v-bind(shadeColor), v-bind(primaryColor));
+    background-image: linear-gradient(to right top, v-bind(shadeColor), v-bind(secondaryColor));
     clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%);
 }
 
 .bottom-fragment{
-    background-image: linear-gradient(to right top, v-bind(shadeColor), v-bind(primaryColor));
+    background-image: linear-gradient(to right top, v-bind(shadeColor), v-bind(secondaryColor));
     clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%);
 }
 </style>
