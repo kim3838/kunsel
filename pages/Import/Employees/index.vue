9e6ce0bf4dab996cf47ed2e93922f0aaee36cb47
Kim@PC <kimdeguzman20@yahoo.com>
2025-08-04 23:30:08 +0800

    Enhance Employee Import with Validation and Success Handling
    - Updated file input component with new icon, size, and ref for binding.
    - Added validation error display with detailed DataTable and icons.
    - Implemented success response handling with imported employee records table.
    - Extended upload function to handle errors and successful imports effectively.
    - Introduced reactive variables for managing states and dynamically rendering validations and results.

--- pages/Import/Employees/index.vue
+++ pages/Import/Employees/index.vue
@@ -5,7 +5,7 @@
                 <div class="space-y-2 p-[20px]">
                     <div class="grid gap-2 grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
                         <div class="col-span-3">
-                            <InputWithIcon :icon="'ic:baseline-file-present'" type="file" />
+                            <InputWithIcon ref="fileToImport" :size="'md'" class="mt-2" :icon="'ic:baseline-file-open'" type="file" />
                         </div>
                     </div>
                     <div class="space-x-1">
@@ -13,7 +13,53 @@
                             <Button class="inline-block" :icon="'ic:baseline-download'" :size="'sm'" :disabled="disableActions" :label="'CSV Template'"/>
                         </NuxtLink>
                         <Button class="inline-block" :icon="'ic:baseline-upload'" :size="'sm'" :disabled="disableActions" :label="uploadLabel"  @click="upload"/>
-                        <Button class="inline-block" :icon="'ic:baseline-save'" :size="'sm'" :disabled="disableActions" :label="saveLabel"  @click="save"/>
+                    </div>
+
+                    <UnorderedList
+                        v-if="disableActions"
+                        :icon="'eos-icons:loading'"
+                        :size="'md'"
+                        :label="'Please wait...'"/>
+
+                    <div v-if="hasValidationError" class="pt-4 space-y-2">
+                        <div class="font-header">
+                            <p class="font-medium text-lg">Errors</p>
+                        </div>
+                        <DataTable
+                            :headers="validationErrorHeaders"
+                            :size="'lg'"
+                            :rows="validationErrorsData"
+                            :disabled="disableActions"
+                            :selection="false">
+                            <template v-slot:cell.column="{cell, slot, scrollReference}">
+                                <div class="p-[3px]">
+                                    {{cell.attribute}}
+                                </div>
+                            </template>
+                            <template v-slot:cell.errors="{cell, slot, scrollReference}">
+                                <div class="p-[3px]">
+                                    <UnorderedList v-for="error in cell.errors" :key="error" :label="error" />
+                                </div>
+                            </template>
+                            <template v-slot:cell.values="{cell, slot, scrollReference}">
+                                <div class="p-[3px]">
+                                    {{cell.values[cell.attribute]}}
+                                </div>
+                            </template>
+                        </DataTable>
+                    </div>
+
+                    <div v-if="importSuccessful" class="pt-4 space-y-2">
+                        <div class="font-header">
+                            <p class="font-medium text-lg">Successfully imported {{importedEmployeesCount}} employee records</p>
+                        </div>
+                        <DataTable
+                            :headers="importedEmployeeHeaders"
+                            :size="'lg'"
+                            :rows="importedEmployeesData"
+                            :disabled="disableActions"
+                            :selection="false">
+                        </DataTable>
                     </div>
                 </div>
             </div>
@@ -29,8 +75,18 @@ definePageMeta({middleware: ['auth', 'admin-of-selected-company']});
 useLayout().setNavigationMode('solid');
 
 const {baseURL} = useRuntimeConfig().public;
+const {isAuthenticated} = useAuth();
+const nuxtApp = useNuxtApp();
+const coreStore = useCoreStore();
+const {
+    updatedAssociatedCompanyFlag
+} = storeToRefs(nuxtApp.$associationStore);
+const {
+    selectedAssociatedCompany
+} = storeToRefs(nuxtApp.$authStore);
 
 const pending = ref(false);
+const fileToImport = ref(null);
 
 const uploadLabel = computed(() => {
     return "Upload";
@@ -57,8 +113,104 @@ const downloadTemplate = async () => {
         }
     );
 }
-const upload = () => {}
-const save = () => {}
+
+const validationErrorHeaders = reactive<TableHeaderT[]>([
+    { text: 'Row', value: 'row', alignData: 'left'},
+    { text: 'Column', value: 'column', alignData: 'left'},
+    { text: 'Value', value: 'values', alignData: 'left'},
+    { text: 'Errors', value: 'errors', alignData: 'left'},
+]);
+
+const validationErrorsData = ref([]);
+const hasValidationError = computed(() => {
+    return validationErrorsData.value.length > 0;
+});
+
+const importedEmployeeHeaders = reactive<TableHeaderT[]>([
+    { text: 'Number', value: 'number', alignData: 'left'},
+    { text: 'Family name', value: 'family_name', alignData: 'left'},
+    { text: 'Given name', value: 'given_name', alignData: 'left'}
+]);
+const importedEmployeesData = ref([]);
+const importSuccessful = computed(() => {
+    return importedEmployeesCount.value > 0;
+});
+const importedEmployeesCount = computed(() => {
+    return importedEmployeesData.value.length;
+})
+
+watch(updatedAssociatedCompanyFlag, (newValue) => {
+    if(isAuthenticated.value){
+        validationErrorsData.value = [];
+        importedEmployeesData.value = [];
+    }
+})
+
+const upload = async () => {
+
+    let file = fileToImport.value.$refs.input.files[0];
+
+    if (file) {
+        pending.value = true;
+        validationErrorsData.value = [];
+        importedEmployeesData.value = [];
+
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('company_id', selectedAssociatedCompany.value);
+
+        await laraFetch("/api/employee-pre-import", {
+            method: 'POST',
+            body: formData,
+        },{
+            onRequestError: (request, options, error) => {
+                pending.value = false;
+
+                coreStore.setServiceError({
+                    prompt: true,
+                    icon: 'mdi:information-variant-circle-outline',
+                    title: 'Request failed',
+                    payload: {
+                        message: 'Re-attach the file if its edited.'
+                    }
+                });
+
+                setTimeout(() => {
+
+                }, 10);
+            },
+            onResponse: () => {
+                pending.value = false;
+            },
+            onUnprocessableContentResponse: async (request, options, response) => {
+                validationErrorsData.value = _get(response, '_data.errors', []);
+
+                coreStore.setServiceError({
+                    prompt: true,
+                    icon: 'mdi:information-variant-circle-outline',
+                    title: 'Request failed',
+                    payload: {
+                        message: _get(response, '_data.message', 'Validation failed.')
+                    }
+                });
+            },
+            onSuccessResponse: async (request, options, response) => {
+                importedEmployeesData.value = _get(response, '_data.values.employees', []);
+            }
+        }, true);
+
+
+    } else {
+        coreStore.setServiceError({
+            prompt: true,
+            icon: 'mdi:information-variant-circle-outline',
+            title: 'Request failed',
+            payload: {
+                message: 'File not found.'
+            }
+        });
+    }
+}
 </script>
 
 <style scoped>
