e201941805c92a9910908d2c77feb275f49e0404
Kim@PC <kimdeguzman20@yahoo.com>
2025-09-11 22:31:53 +0800

    Include Fetch Filters in MultiSelectPaginated Request
    - Added support to pass `props.payload.fetch.filters` into request filters for improved query flexibility.

--- components/form/MultiSelectPaginated.vue
+++ components/form/MultiSelectPaginated.vue
@@ -503,6 +503,7 @@ let paramsComputed = computed(() => {
         page: page.value,
         perPage: perPage.value,
         filters: {
+            ...props.payload.fetch.filters,
             search: props.payload.fetch.filters.search.keyword
         }
     };
