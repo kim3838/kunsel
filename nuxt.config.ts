ee3539973f86c8f48473aef133efce57bf3ecb41
Kim@PC <kimdeguzman20@yahoo.com>
2025-10-07 17:03:21 +0800

    Remove @nuxt/fonts and Redundant Font Management
    - Removed `@nuxt/fonts` module and its configuration from `nuxt.config.ts` for a streamlined setup.
    - Transferred font definitions directly to the `head.link` section for improved loading control.
    - Deleted `@nuxt/fonts` dependency from `package.json` and `package-lock.json` to reduce overhead.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,41 @@ export default defineNuxtConfig({
                 {"name": "viewport", "content": "width=device-width, initial-scale=1"},
                 {"charset": "utf-8"}
             ],
-            "link": [],
+            "link": [
+                {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+                {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'use-credentials'},
+                //Sans-Serif
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Sofia+Sans+Semi+Condensed:ital,wght@0,1;0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,1;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Hedvig+Letters+Sans&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap" rel" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Radio+Canada+Big:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=National+Park:wght@200..800&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet'},
+                //Serif
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oranienbaum&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap'},
+                //Monospace
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Spline+Sans+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'},
+                //Stiff
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Iceberg&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap'},
+                //Techno
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap'},
+                //Appearance
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&display=swap'},
+                //Sophisticated
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Quintessential&display=swap'},
+            ],
             "style": [],
             "script": [
                 {src: '/js/common/moment.min.js'},
@@ -109,51 +143,8 @@ export default defineNuxtConfig({
         '@vueuse/nuxt',
         '@pinia/nuxt',
         //'nuxt-lodash'
-        '@nuxt/icon',
-        '@nuxt/fonts'
+        '@nuxt/icon'
     ],
-
-    fonts: {
-        families: [
-            //Sans-Serif
-            { name: 'Inter', provider: 'google' },
-            { name: 'Roboto', provider: 'google' },
-            { name: 'Roboto Condensed', provider: 'google' },
-            { name: 'Sofia Sans Semi Condensed', provider: 'google' },
-            { name: 'IBM Plex Sans Condensed', provider: 'google' },
-            { name: 'Inclusive Sans', provider: 'google' },
-            { name: 'Protest Revolution', provider: 'google' },
-            { name: 'Hedvig Letters Sans', provider: 'google' },
-            { name: 'Bricolage Grotesque', provider: 'google' },
-            { name: 'Radio Canada Big', provider: 'google' },
-            { name: 'National Park', provider: 'google' },
-            { name: 'Funnel Sans', provider: 'google' },
-
-            //Serif
-            { name: 'Instrument Serif', provider: 'google' },
-            { name: 'Oranienbaum', provider: 'google' },
-            { name: 'Almendra', provider: 'google' },
-            { name: 'Playfair Display', provider: 'google' },
-
-            //Monospace
-            { name: 'Roboto Mono', provider: 'google' },
-            { name: 'Spline Sans Mono', provider: 'google' },
-
-            //Stiff
-            { name: 'Iceberg', provider: 'google' },
-            { name: 'Bruno Ace SC', provider: 'google' },
-
-            //Techno
-            { name: 'Oxanium', provider: 'google' },
-
-            //Appearance
-            { name: 'Roboto Flex', provider: 'google' },
-            { name: 'Merienda', provider: 'google' },
-
-            //Sophisticated
-            { name: 'Quintessential', provider: 'google' },
-        ]
-    },
 
     vite: {
         css: {
