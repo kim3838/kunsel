b0a230951f3d0ac9ae0332d391a6a31cf0aa60df
Kim@PC <kimdeguzman20@yahoo.com>
2025-10-15 13:24:49 +0800

    Add New Component Instance Type Definitions
    - Introduced `ShiftAssignmentsInstance`, `EmployeeSelectionInstance`, `ShiftSelectionInstance`, and `ShiftByEmployeeSelectionInstance` interfaces.
    - Added `paginate`, `clearFlags`, `clearData`, and `reset` method definitions for improved type safety and functionality.

--- public/js/types/component-instance.ts
+++ public/js/types/component-instance.ts
@@ -2,3 +2,25 @@
 export interface MultiSelectPaginatedInstance {
     clearSelected: () => void;
 }
+
+export interface ShiftAssignmentsInstance {
+    paginate: (page?: number, clearSelection?: boolean) => Promise<void>;
+}
+
+export interface EmployeeSelectionInstance {
+    clearFlags: () => void;
+    clearData: () => void;
+    paginate: (page?: number, clearSelection?: boolean) => Promise<void>;
+    reset: () => void;
+}
+
+export interface ShiftSelectionInstance {
+    paginate: (page?: number, clearSelection?: boolean) => Promise<void>;
+}
+
+export interface ShiftByEmployeeSelectionInstance {
+    clearFlags: () => void;
+    clearData: () => void;
+    paginate: (page?: number, clearSelection?: boolean) => Promise<void>;
+    reset: () => void;
+}
