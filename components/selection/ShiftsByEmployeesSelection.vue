885d47b151cec657919df94cc613e5d434e8baf4
Kim@PC <kimdeguzman20@yahoo.com>
2025-10-16 14:18:19 +0800

    Replace Lazy Multi-Select With Standard Multi-Select
    - Substituted `LazyMultiSelectPaginated` with `MultiSelectPaginated` in `ShiftsByEmployeesSelection`.
    - Removed `hydrate-on-interaction` property as it's no longer applicable.

--- components/selection/ShiftsByEmployeesSelection.vue
+++ components/selection/ShiftsByEmployeesSelection.vue
@@ -31,8 +31,7 @@
             <div class="grid gap-2 grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
                 <div class="col-span-2">
                     <InputLabel :size="'sm'" value="Assigned Shift" />
-                    <LazyMultiSelectPaginated
-                        hydrate-on-interaction="mouseover"
+                    <MultiSelectPaginated
                         :icon="'tdesign:component-checkbox'"
                         :disabled="disableActions"
                         glint
@@ -44,8 +43,7 @@
                 </div>
                 <div class="col-span-2">
                     <InputLabel :size="'sm'" value="Not Assigned Shift" />
-                    <LazyMultiSelectPaginated
-                        hydrate-on-interaction="mouseover"
+                    <MultiSelectPaginated
                         :icon="'tdesign:component-checkbox'"
                         :disabled="disableActions"
                         glint
