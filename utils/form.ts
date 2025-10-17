d6e51872222a8e1c85b6c1f31441c299829c4cda
Kim@PC <kimdeguzman20@yahoo.com>
2025-10-17 15:32:34 +0800

    Add `SELECT` Enum to Utilities
    - Created `SELECT` enum in `form.ts` for numeric-based selection types.
    - Applied `NumericEnumInterface` for consistent type definition.
    - Defined selection types: `SINGLE_STATIC`, `MULTI_STATIC`, `SINGLE_PAGINATED`, and `MULTI_PAGINATED`.

---
+++ utils/form.ts
@@ -0,0 +1,9 @@
+
+import type {NumericEnumInterface} from "@/public/js/common/type";
+
+export const SELECT: NumericEnumInterface = {
+    SINGLE_STATIC: 0,
+    MULTI_STATIC: 1,
+    SINGLE_PAGINATED: 3,
+    MULTI_PAGINATED: 4,
+}
