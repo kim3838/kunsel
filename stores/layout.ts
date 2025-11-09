0c0866240b54fd7aa23b1266fe491b6c3fbf6e6f
Kim@PC <kimdeguzman20@yahoo.com>
2025-11-09 12:19:36 +0800

    Update Layout Store With Sub-Navigation Drop Align Support
    - Added `subNavigationDropAlign` state with default value `'left'`.
    - Introduced `setSubNavigationDropAlign` function to update alignment dynamically.

--- stores/layout.ts
+++ stores/layout.ts
@@ -3,14 +3,20 @@ import {defineStore} from 'pinia'
 import type {NavigationLinkInterface} from "@/public/js/types/layout";
 
 export const useLayoutStore = defineStore('layout', () => {
+    const subNavigationDropAlign = ref('left');
     const subNavigationOptions = ref<NavigationLinkInterface[]>([]);
 
+    function setSubNavigationDropAlign(align: string){
+        subNavigationDropAlign.value = align;
+    }
     function setSubNavigationOptions(options: NavigationLinkInterface[]){
         subNavigationOptions.value = options;
     }
 
     return {
+        subNavigationDropAlign,
         subNavigationOptions,
+        setSubNavigationDropAlign,
         setSubNavigationOptions
     }
 })
