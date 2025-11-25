739f01b154075010599225942ef9327cc9a9051d
Kim@PC <kimdeguzman20@yahoo.com>
2025-11-25 11:08:50 +0800

    Refactor and Enhance Debugging in `use-diagnostics`
    - Added `userFlags` object to track authentication and user roles.
    - Introduced `debugCorePluginProvides`, `debugLibraries`, and `debugConfigs` functions for modular debugging.
    - Enhanced readability and maintainability of the debugging logic.

--- composables/utility/use-diagnostics.ts
+++ composables/utility/use-diagnostics.ts
@@ -8,18 +8,38 @@ export const useDiagnostics = () => {
     const runtimeConfig = useRuntimeConfig();
     const appConfig = useAppConfig();
 
-    const performFundamentals = async () => {
+    const {isAuthenticated, userIsSuperAdmin} = useAuth();
+    const userIsAdminInAnyCompany = userIsAdminInAnyCompanyState();
+    const userIsAdminOfSelectedCompany = userIsAdminOfSelectedCompanyState();
+    const userIsEmployeeOfSelectedCompany = userIsEmployeeOfSelectedCompanyState();
+    const userFlags = {
+        is_authenticated: isAuthenticated.value,
+        user_is_super_admin: userIsSuperAdmin.value,
+        user_is_admin_in_any_company: userIsAdminInAnyCompany.value,
+        user_is_admin_of_selected_company: userIsAdminOfSelectedCompany.value,
+        user_is_employee_of_selected_company: userIsEmployeeOfSelectedCompany.value
+    }
 
+    const debugCorePluginProvides = () => {
         Array.from({ length: 25 }, (_, i) => _debug(`${i} TO ROMAN NUMERAL: `,toRomanNumeral(i)));
         Array.from({ length: 25 }, (_, i) => _debug(`${i} ORDINAL: `,ordinal(i)));
-        _debug('runtimeConfig',runtimeConfig);
-        _debug('appConfig', appConfig);
-        _debug('NODE_ENV', process.env.NODE_ENV);
+    }
+
+    const debugLibraries = async () => {
         _debug('moment', $moment().format('YYYY-MM-DD'));
         _debug('lodash', _upperCase('lodash uppercase'));
     }
 
+    const debugConfigs = async () => {
+        _debug('runtimeConfig',runtimeConfig);
+        _debug('appConfig', appConfig);
+        _debug('NODE_ENV', process.env.NODE_ENV);
+    }
+
     return {
-        performFundamentals
+        userFlags,
+        debugLibraries,
+        debugCorePluginProvides,
+        debugConfigs,
     };
 }
