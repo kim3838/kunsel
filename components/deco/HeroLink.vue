5ea206f030d75aa85ac20e18e4d3e2332cea1a14
Kim@PC <kimdeguzman20@yahoo.com>
2025-11-25 11:01:05 +0800

    Update Border Thickness in `HeroLink` Component
    - Changed `border` from `2px` to `1px` for a sleeker design.

--- components/deco/HeroLink.vue
+++ components/deco/HeroLink.vue
@@ -15,7 +15,7 @@
     display: block;
     padding: 12px 30px;
     text-decoration: none;
-    border: 2px solid;
+    border: 1px solid;
     border-image-slice: 1;
     border-image-source: linear-gradient(45deg, #7d7d7f 20%, #fefdfd, #3d3d3e 70%, #fefdfd, #7d7d7f 100%);
     transition: all 1s cubic-bezier(0.645, 0.045, 0.355, 1);
