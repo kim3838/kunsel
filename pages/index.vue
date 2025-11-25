95f5e8ccae1cfebca5b789b50560a05158179929
Kim@PC <kimdeguzman20@yahoo.com>
2025-11-25 11:59:24 +0800

    Remove Placeholder HeroLink From Index Page
    - Deleted "Dashboard coming soon..." HeroLink section for cleaner layout.

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,6 @@
         <DefaultWrapper>
             <div class="mx-auto max-w-screen-2xl">
 
-                <div class="my-20 flex justify-center">
-                    <HeroLink><span class="font-business">Dashboard coming soon...</span></HeroLink>
-                </div>
-
             </div>
         </DefaultWrapper>
     </div>
