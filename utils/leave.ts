33ae1dacbb44da6fd82ea6dfe4a05d73af3b0bed
Kim@PC <kimdeguzman20@yahoo.com>
2025-12-02 08:50:10 +0800

    Add Leave Type and Usage Enumerations
    - Introduced `LEAVE_TYPE`, `LEAVE_USAGE_SPAN_TYPE`, `LEAVE_PERIOD_TYPE`, `LEAVE_INTERVAL_SPAN_TYPE`, and `LEAVE_CARRY_OVER_TYPE` constant enumerations for defining leave-related categories.
    - Added corresponding name mappings for each enumeration to improve readability and type usage consistency.
    - Imported `NumericEnumInterface` and `StringEnumInterface` from `common/type` for structure standardization.

---
+++ utils/leave.ts
@@ -0,0 +1,60 @@
+
+import type {NumericEnumInterface, StringEnumInterface} from "@/public/js/common/type";
+
+export const  LEAVE_TYPE: NumericEnumInterface = {
+    VACATION : 100,
+    SICK : 200,
+    EMERGENCY : 300,
+}
+
+export const LEAVE_TYPE_NAME: StringEnumInterface = {
+    [LEAVE_TYPE.VACATION as number]: 'Vacation',
+    [LEAVE_TYPE.SICK as number]: 'Sick',
+    [LEAVE_TYPE.EMERGENCY as number]: 'Emergency',
+};
+
+export const  LEAVE_USAGE_SPAN_TYPE: NumericEnumInterface = {
+    DAY : 100,
+    MONTH : 200,
+    YEAR : 300,
+}
+
+export const LEAVE_USAGE_SPAN_TYPE_NAME: StringEnumInterface = {
+    [LEAVE_USAGE_SPAN_TYPE.DAY as number]: 'Day',
+    [LEAVE_USAGE_SPAN_TYPE.MONTH as number]: 'Month',
+    [LEAVE_USAGE_SPAN_TYPE.YEAR as number]: 'Year',
+};
+
+export const  LEAVE_PERIOD_TYPE: NumericEnumInterface = {
+    INTERVAL : 100,
+    CALENDAR_YEAR : 200,
+}
+
+export const LEAVE_PERIOD_TYPE_NAME: StringEnumInterface = {
+    [LEAVE_PERIOD_TYPE.INTERVAL as number]: 'Interval',
+    [LEAVE_PERIOD_TYPE.CALENDAR_YEAR as number]: 'Calendar year',
+};
+
+export const  LEAVE_INTERVAL_SPAN_TYPE: NumericEnumInterface = {
+    DAY : 100,
+    MONTH : 200,
+    YEAR : 300,
+}
+
+export const LEAVE_INTERVAL_SPAN_TYPE_NAME: StringEnumInterface = {
+    [LEAVE_INTERVAL_SPAN_TYPE.DAY as number]: 'Day',
+    [LEAVE_INTERVAL_SPAN_TYPE.MONTH as number]: 'Month',
+    [LEAVE_INTERVAL_SPAN_TYPE.YEAR as number]: 'Year',
+};
+
+export const  LEAVE_CARRY_OVER_TYPE: NumericEnumInterface = {
+    ALL : 100,
+    LIMIT : 200,
+    PERCENTAGE : 300,
+}
+
+export const LEAVE_CARRY_OVER_TYPE_NAME: StringEnumInterface = {
+    [LEAVE_CARRY_OVER_TYPE.ALL as number]: 'All',
+    [LEAVE_CARRY_OVER_TYPE.LIMIT as number]: 'Limit',
+    [LEAVE_CARRY_OVER_TYPE.PERCENTAGE as number]: 'Percentage',
+};
