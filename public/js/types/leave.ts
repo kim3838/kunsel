96b8c35a820a9fe2a185c528067ecd18b990efa3
Kim@PC <kimdeguzman20@yahoo.com>
2025-12-04 11:17:48 +0800

    Add Leave Types and Balance Type Definitions
    - Introduced `LeaveTypeT` type definition to standardize leave type properties and structure.
    - Added `LeaveTypeBalancePerPeriodT` type for managing balance data across periods.

---
+++ public/js/types/leave.ts
@@ -0,0 +1,39 @@
+
+import type {EnumOption} from "@/public/js/common/type";
+
+export type LeaveTypeT = {
+    id: string | number,
+    ulid: string,
+    company_id: string | number,
+    code: string,
+    name: string,
+    type: EnumOption,
+    is_paid: boolean,
+    monetizable: boolean,
+
+    limit_usage: boolean,
+    limit_usage_span_type: EnumOption,
+    limit_usage_span_value: number,
+    limit_usage_value: number
+
+    eligibility_employment_types: number[] | null,
+    initial_balance_upon_eligibility: number,
+
+    period_type: EnumOption,
+    period_interval_span_type: EnumOption | null,
+    period_interval_span_value: number,
+    period_calendar_span_value: number,
+
+    carry_over_balance_per_new_period: boolean,
+    carry_over_balance_type: EnumOption | null,
+    carry_over_balance_value: number,
+}
+
+export type LeaveTypeBalancePerPeriodT = {
+    id?: number,
+    leave_type_id: number | null,
+    from_period: number,
+    and_so_on: boolean,
+    to_period: number | null,
+    balance: number,
+}
