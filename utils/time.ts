b32095b796a6328d4524541ba57091065da1c61a
Kim@PC <kimdeguzman20@yahoo.com>
2025-12-04 11:17:39 +0800

    Add Month Enum and Month Names Mapping
    - Introduced `MONTH` numeric enum for months mapping.
    - Added `MONTH_NAMES` string enum for month names corresponding to numeric values.

--- utils/time.ts
+++ utils/time.ts
@@ -20,3 +20,33 @@ export const WEEK_DAY_NAMES: StringEnumInterface = {
     [WEEK_DAY.FRIDAY as number]: 'Friday',
     [WEEK_DAY.SATURDAY as number]: 'Saturday',
 }
+
+export const  MONTH: NumericEnumInterface = {
+    JANUARY : 1,
+    FEBRUARY: 2,
+    MARCH: 3,
+    APRIL: 4,
+    MAY: 5,
+    JUNE: 6,
+    JULY: 7,
+    AUGUST: 8,
+    SEPTEMBER: 9,
+    OCTOBER: 10,
+    NOVEMBER: 11,
+    DECEMBER: 12,
+}
+
+export const MONTH_NAMES: StringEnumInterface = {
+    [MONTH.JANUARY as number]: 'January',
+    [MONTH.FEBRUARY as number]: 'February',
+    [MONTH.MARCH as number]: 'March',
+    [MONTH.APRIL as number]: 'April',
+    [MONTH.MAY as number]: 'May',
+    [MONTH.JUNE as number]: 'June',
+    [MONTH.JULY as number]: 'July',
+    [MONTH.AUGUST as number]: 'August',
+    [MONTH.SEPTEMBER as number]: 'September',
+    [MONTH.OCTOBER as number]: 'October',
+    [MONTH.NOVEMBER as number]: 'November',
+    [MONTH.DECEMBER as number]: 'December',
+}
