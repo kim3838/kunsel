e16dd71d056f8ec359b16bcf0d170f64675afffb
Kim@PC <kimdeguzman20@yahoo.com>
2025-12-30 14:15:16 +0800

    Add Company ID Validation to Designations Page
    - Updated middleware in `Designations.vue` to validate `company_id` via API for access control.
    - Enhanced error handling for empty or unauthorized responses during middleware execution.
    - Included `company_id` in API request parameters for accurate data retrieval.

--- pages/Workforce/Designations.vue
+++ pages/Workforce/Designations.vue
@@ -124,7 +124,19 @@ import {storeToRefs} from "pinia";
 import type {EnumOption, EnumSelection} from "@/public/js/common/type";
 
 useHead({titleTemplate: (titleChunk) => {return `${titleChunk} - Designations`}});
-definePageMeta({middleware: ['auth', 'admin-of-selected-company']});
+definePageMeta({middleware: ['auth', 'admin-of-selected-company',
+    async () => {
+
+        const {selectedAssociatedCompanyId} = storeToRefs(useAuthStore());
+        const {data, error} = await laraUseFetch(`/api/designations-gate`, {method: 'GET', params: {company_id: selectedAssociatedCompanyId.value}}, {}, false);
+
+        if(_isEmpty(data.value) && !_isEmpty(error.value)){
+            let responseCode = _get(error.value, 'data.code', null);
+
+            throw createError({ statusCode: responseCode, statusMessage: useCoreStore().servicePayloadMessage, fatal: true});
+        }
+    }
+]});
 useLayout().setNavigationMode('solid');
 
 const {isAuthenticated} = useAuth();
@@ -188,6 +200,7 @@ const designationsExecute = async () => {
     await laraFetch("/api/designations", {
         method: 'GET',
         params: {
+            company_id: selectedAssociatedCompanyId.value,
             filters: {
                 'company_id': selectedAssociatedCompanyId.value,
                 'search' : filters.search.keyword,
