ede17cfc939993b67d4321b3b4dd51e33e0a2c5b
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-04 13:48:08 +0800

    Refactor Subscription Module and Plan Declarations
    - Moved `SUBSCRIPTION_MODULE` and `SUBSCRIPTION_MODULE_NAME` constants from `core.ts` to `account.ts` for centralized management.
    - Renamed `ACCOUNT_PLAN` to `SUBSCRIPTION_PLAN` for consistency.
    - Added `SUBSCRIPTION_PLAN_NAME` constant in `account.ts` for mapping plan names to their numeric values.

--- utils/account.ts
+++ utils/account.ts
@@ -1,7 +1,26 @@
 
-import type {NumericEnumInterface} from "@/public/js/common/type";
+import type {NumericEnumInterface, StringEnumInterface} from "@/public/js/common/type";
 
-export const  ACCOUNT_PLAN: NumericEnumInterface = {
+export const  SUBSCRIPTION_PLAN: NumericEnumInterface = {
     STANDARD : 100,
     BUSINESS : 500,
+}
+
+export const  SUBSCRIPTION_PLAN_NAME: StringEnumInterface = {
+    [SUBSCRIPTION_PLAN.STANDARD as number] : 'Standard',
+    [SUBSCRIPTION_PLAN.BUSINESS as number] : 'Business',
+}
+
+export const  SUBSCRIPTION_MODULE: NumericEnumInterface = {
+    EMPLOYEE_PORTAL : 50,
+    HR_PAYROLL : 100,
+    INVENTORY : 200,
+    FINANCE_ACCOUNTING : 300,
+}
+
+export const  SUBSCRIPTION_MODULE_NAME: StringEnumInterface = {
+    [SUBSCRIPTION_MODULE.EMPLOYEE_PORTAL as number] : 'Employee Portal',
+    [SUBSCRIPTION_MODULE.HR_PAYROLL as number] : 'HR & Payroll',
+    [SUBSCRIPTION_MODULE.INVENTORY as number] : 'Inventory',
+    [SUBSCRIPTION_MODULE.FINANCE_ACCOUNTING as number] : 'Finance & Accounting',
 }
