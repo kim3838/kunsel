b5cbb532edb5593496599537bf2a89fc361c424b
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-09 20:29:03 +0800

    Add useAccount Composable
    - Created `useAccount` composable to manage account-related functionality.
    - Introduced `storeAccount` and `persistAccount` methods for handling account data persistently using cookies.
    - Ensured compatibility with runtime configuration and app context.

---
+++ composables/use-account.ts
@@ -0,0 +1,29 @@
+export const useAccount = () => {
+    const createAccountCookie = () => {
+        const {$authStore} = useNuxtApp();
+        const {sessionDomain} = useRuntimeConfig().public;
+
+        return useCookie<number>($authStore.SELECTED_ASSOCIATED_ACCOUNT_STORAGE_KEY, {
+            domain: sessionDomain,
+            sameSite: 'lax',
+        });
+    };
+
+    const storeAccount = (value: number) => {
+        const storedAccount = createAccountCookie();
+
+        if ((!Number.isNaN(value) && typeof value === 'number') && storedAccount.value !== value) {
+            storedAccount.value = value;
+        }
+    };
+
+    const persistAccount = computed(() => {
+        const storedAccount = createAccountCookie();
+        return storedAccount.value;
+    });
+
+    return {
+        storeAccount,
+        persistAccount
+    };
+};
