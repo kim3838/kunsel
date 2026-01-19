972820822782883517edcc2b0a2f7b41a1460a78
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-19 15:52:49 +0800

    Enhance Approval State Sub-Row Component
    - Updated `ApprovalStateSubRow` to use `rowsProxy` for computed row data with label shading.
    - Added multiple cells (e.g., `username`, `user_status`, `company_employee_number`) for detailed approver information.
    - Integrated label component in the `status` cell for dynamic shading based on status.
    - Refined `approversHeaders` configuration to include new fields and column adjustments.
    - Organized `rowsProxy` logic to handle status-based shading using `REQUEST_APPROVAL_STATUS`.

--- components/data/ApprovalStateSubRow.vue
+++ components/data/ApprovalStateSubRow.vue
@@ -5,9 +5,29 @@
             :headers="approversHeaders"
             :disabled="disabled"
             :size="'md'"
-            :rows="rows">
+            :rows="rowsProxy">
             <template v-slot:cell.status="{cell,slot}">
-                <div class="p-[3px]">{{cell.status?.text}}</div>
+                <div class="flex space-x-1 px-[0.3rem] items-center">
+                    <Label :size="slot.labelSize" :type="cell?._payload?.label_shade?.value as LabelTypeT" shade :label="cell.status?.text" />
+                </div>
+            </template>
+
+            <template v-slot:cell.user_status="{cell,slot}">
+                <div class="p-[3px]">{{cell.approver.status?.text}}</div>
+            </template>
+            <template v-slot:cell.username="{cell,slot}">
+                <div class="p-[3px]">{{cell.approver.username}}</div>
+            </template>
+            <template v-slot:cell.company_employee_number="{cell,slot}">
+                <div class="p-[3px]">{{cell.approver.company_employee_number}}</div>
+            </template>
+            <template v-slot:cell.company_employee_full_name="{cell,slot}">
+                <div class="p-[3px]">{{cell.approver.company_employee_full_name}}</div>
+            </template>
+            <template v-slot:cell.company_assignment_type="{cell,slot}">
+                <div class="px-[3px]">
+                    <span v-if="cell.approver.company_assignment_type?.value">{{cell.approver.company_assignment_type?.text}}</span>
+                </div>
             </template>
         </DataTable>
     </div>
@@ -15,6 +35,7 @@
 
 <script setup lang="ts">
 import type {TableHeaderT, TableRowT} from "@/public/js/types/data";
+import type {LabelTypeT} from "@/public/js/types/theme";
 
 const props = defineProps({
     rows: {
@@ -27,15 +48,51 @@ const props = defineProps({
     },
 });
 
+const rowsProxy = computed({
+    get() {
+        return props.rows.map((approvalState: TableRowT) => {
+
+            let statusSummary = _get(approvalState, 'status.value', 0);
+
+            let shade = 'default';
+
+            if(statusSummary == REQUEST_APPROVAL_STATUS.DECLINED){
+                shade = 'danger';
+            } else if(statusSummary == REQUEST_APPROVAL_STATUS.APPROVED){
+                shade = 'success';
+            }
+
+            return {
+                ...approvalState,
+                _payload: {
+                    'label_shade': {
+                        'cell': ['status'],
+                        'value': shade
+                    }
+                }
+            };
+        });
+    },
+    set(newValue) {
+
+    }
+});
+
 const emit = defineEmits(['proxyEdit', 'syncSelected']);
 
 const rowsIsEmpty = computed(() => props.rows.length === 0);
 
 const approversHeaders = reactive<TableHeaderT[]>([
-    { text: 'Order', value: 'order', alignData: 'left'},
-    { text: 'Username', value: 'approver', alignData: 'left'},
-    { text: 'Remarks', value: 'remarks', alignData: 'left'},
-    { text: 'Approval status', value: 'status', alignData: 'left'},
+    { text: 'Order', value: 'order'},
+    { text: 'Status', value: 'status'},
+    { text: '', value: 'current_state_message', minWidth: '54.11px'},
+    { text: 'Remarks', value: 'remarks'},
+
+    { text: 'Status', value: 'user_status'},
+    { text: 'Username', value: 'username'},
+    { text: 'Employee #', value: 'company_employee_number'},
+    { text: 'Name', value: 'company_employee_full_name'},
+    { text: 'Company Assignment', value: 'company_assignment_type'},
 ]);
 </script>
 
