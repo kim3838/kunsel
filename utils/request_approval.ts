0d22387a59401eda47f85dfbb0e6c922cb389e77
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-24 08:51:46 +0800

    Move Approval Enums to Request Approval Utility
    - Relocated `APPROVER_TYPE`, `APPROVER_TYPE_NAME`, `REQUEST_APPROVAL_STATUS`, and `REQUEST_APPROVAL_STATUS_NAME` from `approval_setting.ts` to `request_approval.ts` for consolidation.
    - Removed `approval_setting.ts` as it is no longer needed.

--- utils/request_approval.ts
+++ utils/request_approval.ts
@@ -1,6 +1,30 @@
 
 import type {NumericEnumInterface, StringEnumInterface} from "@/public/js/common/type";
 
+export const APPROVER_TYPE: NumericEnumInterface = {
+    SELECTED : 0,
+    DEPARTMENT_HEAD: 100,
+    MANAGER: 200
+}
+
+export const APPROVER_TYPE_NAME: StringEnumInterface = {
+    [APPROVER_TYPE.SELECTED as number]: 'Selected',
+    [APPROVER_TYPE.DEPARTMENT_HEAD as number]: 'Department Head',
+    [APPROVER_TYPE.MANAGER as number]: 'Manager',
+}
+
+export const REQUEST_APPROVAL_STATUS: NumericEnumInterface = {
+    PENDING : 0,
+    DECLINED: 100,
+    APPROVED: 200
+}
+
+export const REQUEST_APPROVAL_STATUS_NAME: StringEnumInterface = {
+    [REQUEST_APPROVAL_STATUS.PENDING as number]: 'Pending',
+    [REQUEST_APPROVAL_STATUS.DECLINED as number]: 'Declined',
+    [REQUEST_APPROVAL_STATUS.APPROVED as number]: 'Approved',
+}
+
 export const APPROVAL_ACTION: NumericEnumInterface = {
     NOT_SPECIFIED: 0,
     DECLINE: 100,
