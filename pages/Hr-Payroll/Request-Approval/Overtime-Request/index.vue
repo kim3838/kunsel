5bc201c8bea6841127645c64946832d5ddcfb52d
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-26 08:25:22 +0800

    Update Overtime Request Messages and Default Date Handling
    - Corrected validation error message to refer to "overtime request" instead of "attendance adjustment".
    - Updated default `attendanceDate` to use the current date dynamically instead of a hardcoded value.

--- pages/Hr-Payroll/Request-Approval/Overtime-Request/index.vue
+++ pages/Hr-Payroll/Request-Approval/Overtime-Request/index.vue
@@ -721,7 +721,7 @@ const confirmDeleteSelected = () => {
             resetable: false,
             icon: null,
             title: `Validation Error`,
-            message: `No selected attendance adjustment to delete.`,
+            message: `No selected overtime request to delete.`,
             action: {
                 callback: () => {},
                 label: 'Okay'
@@ -1154,7 +1154,7 @@ const loadEditable = () => {
         'ulid': _get(editPayload.value, 'ulid', null),
     };
 
-    attendanceDate.value = _get(editPayload.value, 'date', nuxtApp.$moment('2025-01-10').format("YYYY-MM-DD"));
+    attendanceDate.value = _get(editPayload.value, 'date', nuxtApp.$moment().format("YYYY-MM-DD"));
 
     renderDatePickers();
 }
