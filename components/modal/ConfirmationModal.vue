27448bf19f7a67fe3ed22991c553a1511263cf1e
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-31 19:30:56 +0800

    Update ConfirmationModal and DialogModal Styles for Consistency
    - Replaced `flex-shrink-0` with `shrink-0` in ConfirmationModal for alignment with utility class

--- components/modal/ConfirmationModal.vue
+++ components/modal/ConfirmationModal.vue
@@ -6,14 +6,14 @@
         @close="close">
         <div class="p-3 modal-body">
             <div class="sm:flex sm:items-start">
-                <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 sm:hidden">
+                <div class="mx-auto shrink-0 flex items-center justify-center h-12 w-12 sm:hidden">
                     <Icon v-if="icon" class="h-full w-full" :name="icon"></Icon>
                 </div>
 
                 <div>
                     <div class="flex items-center justify-center gap-1">
                         <Icon v-if="icon" class="hidden sm:inline-block h-8 w-8" :name="icon"></Icon>
-                        <h3 class="text-xl font-medium font-header">
+                        <h3 class="text-lg font-medium font-header">
                             <slot name="title"></slot>
                         </h3>
                     </div>
--- components/modal/DialogModal.vue
+++ components/modal/DialogModal.vue
@@ -9,7 +9,7 @@
         @close="close"
     >
         <div :style=[modalBodySpacingStyle] :class="[bodyBorderBottom ? 'body-bottom-border' : '']">
-            <div class="text-lg font-header">
+            <div class="text-lg font-medium font-header">
                 <slot name="title" />
             </div>
 
