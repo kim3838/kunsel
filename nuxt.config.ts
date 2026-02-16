003403bcb6339eae8885cc80fa65b66f03e89aff
Kim@PC <kimdeguzman20@yahoo.com>
2026-02-16 17:53:56 +0800

    Add Ruluko Font to Nuxt Configuration
    - Included `Ruluko` font in the `head` section of `nuxt.config.ts` for stylesheet loading.
    - Ensured consistent font availability across the application.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,7 @@ export default defineNuxtConfig({
                 {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet'},
                 {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet'},
                 {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet'},
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ruluko&display=swap" rel="stylesheet'},
                 //Serif
                 {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap'},
                 {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oranienbaum&display=swap'},
