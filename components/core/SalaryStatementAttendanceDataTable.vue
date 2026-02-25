f2c00488bf2eb266852aa8e24c29340771717736
Kim@PC <kimdeguzman20@yahoo.com>
2026-02-25 14:00:26 +0800

    Add Day Status to Salary Statement Attendance Data Mapping
    - Introduced `dayStatus` variable to compute and map attendance status values in `SalaryStatementAttendanceDataTable`.

--- components/core/SalaryStatementAttendanceDataTable.vue
+++ components/core/SalaryStatementAttendanceDataTable.vue
@@ -79,6 +79,7 @@ const rowsProxy = computed({
     get() {
         return props.rows.map((salaryStatementAttendance: TableRowT) => {
 
+            let dayStatus = _get(salaryStatementAttendance, 'status.value', 0);
             let dayType = _get(salaryStatementAttendance, 'day_type.value', 0);
 
             let shade = 'clear';
