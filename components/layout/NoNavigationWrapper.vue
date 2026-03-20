6674d24da1a661850ffaf468fe8ad0abedf5f748
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-20 16:04:03 +0800

    Add Salary Statement Page and NoNavigationWrapper Component
    - Created `Salary-Statement/[token].vue` to display salary statement breakdown.
    - Introduced reusable `NoNavigationWrapper` layout component with footer integration.
    - Configured dynamic salary statement fetch URL using route parameters.

---
+++ components/layout/NoNavigationWrapper.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+        v-if="clientReadyState"
+        id="layoutScroll"
+        style="clip-path: inset(0 0 0 0);"
+        class="relative scroll-smooth h-screen max-h-screen overflow-y-scroll snap-none">
+        <!-- Main Content -->
+        <main class="allocate-navigation">
+            <slot></slot>
+        </main>
+        <!-- Footer -->
+        <Footer top-rule-divider landing-links/>
+    </div>
+</template>
+
+<script setup lang="ts">
+const clientReadyState = useClientReadyState();
+const topAllocation = ref('0px');
+</script>
+<style scoped>
+.allocate-navigation {
+    padding-top: v-bind(topAllocation);
+}
+</style>
---
+++ pages/Temp/Salary-Statement/[token].vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <NoNavigationWrapper>
+            <div class="mx-auto max-w-screen-2xl">
+
+                <SalaryStatementBreakdown
+                    class="pt-[20px]"
+                    :salaryStatementFetchUrl="salaryStatementFetchUrl"
+                    v-model:salary-statement-pending="salaryStatementPending" />
+            </div>
+        </NoNavigationWrapper>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+useHead({titleTemplate: (titleChunk) => {return `Salary Statement`}});
+definePageMeta({middleware: 'guest'});
+useLayout().setNavigationMode('solid');
+
+const route = useRoute();
+
+const salaryStatementPending = ref(false);
+const salaryStatementFetchUrl = ref(`/public/salary-statement/${route.params.token}`);
+</script>
+
+<style scoped>
+
+</style>
