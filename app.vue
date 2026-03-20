e2ebcfe2c4c99fb68cb353329359857b106fc577
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-20 09:22:26 +0800

    Refactor Fragment Classes for CSS Simplification
    - Renamed `.top-fragment` to `.t-f` and `.bottom-fragment` to `.b-f` for brevity.
    - Updated class references in `app.vue` to reflect the renamed styles.
    - Adjusted opacity for top fragment to enhance design consistency.

--- app.vue
+++ app.vue
@@ -3,12 +3,12 @@
     <div ref="nuxtWrapper" class="nuxt-page relative">
         <AppRenderingLoading/>
         <div class="absolute inset-x-0 -z-10 overflow-hidden -top-40 sm:-top-80 blur-3xl" aria-hidden="true">
-            <div class="top-fragment relative w-[36.125rem] sm:w-[72.1875rem] left-[calc(50%-11rem)] sm:left-[calc(50%-30rem)] aspect-[1155/678] -translate-x-1/2 rotate-[30deg] opacity-30"></div>
+            <div class="t-f relative w-[36.125rem] sm:w-[72.1875rem] left-[calc(50%-11rem)] sm:left-[calc(50%-30rem)] aspect-[1155/678] -translate-x-1/2 rotate-[30deg] opacity-50"></div>
         </div>
         <NuxtLoadingIndicator />
         <NuxtPage/>
         <div class="absolute inset-x-0 -z-10 overflow-hidden top-[calc(20%)] sm:top-[calc(0%)] blur-3xl" aria-hidden="true">
-            <div class="bottom-fragment relative w-[36.125rem] sm:w-[72.1875rem] left-[calc(50%+3rem)] sm:left-[calc(50%+36rem)] aspect-[1155/678]  -translate-x-1/2"></div>
+            <div class="b-f relative w-[36.125rem] sm:w-[72.1875rem] left-[calc(50%+3rem)] sm:left-[calc(50%+36rem)] aspect-[1155/678]  -translate-x-1/2"></div>
         </div>
     </div>
 </template>
@@ -228,12 +228,12 @@ watch(() => route.name,() => {
     border: 1px dashed v-bind(threadColor);
 }
 
-.top-fragment{
+.t-f{
     background-image: linear-gradient(to right top, v-bind(shadeColor), v-bind(accentColor));
     clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%);
 }
 
-.bottom-fragment{
+.b-f{
     background-image: linear-gradient(to right top, v-bind(shadeColor), v-bind(accentColor));
     clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%);
 }
